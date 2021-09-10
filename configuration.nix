5f71de5f4b38ab7d36b836cb2d14d7af1cf56c1c
Lucas Riutzel <lriutzel@gmail.com>
2021-09-10 16:41:51 -0500

    add udev rule to give logitec devices more time before power management suspends the device

--- configuration.nix
+++ configuration.nix
@@ -280,5 +280,10 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.05"; # Did you read the comment?
+
+  # set logitec mouse to autosuspend after 60 seconds
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTR{idProduct}=="c52b", TEST=="power/control", ATTR{power/control}:="auto", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}:="60000"
+    '';
 }
 
