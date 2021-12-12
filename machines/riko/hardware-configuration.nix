956ac9c3f651e92666c68c85bfc7f5e8300b45f8
Lucas Riutzel <lriutzel@gmail.com>
2021-12-12 17:45:56 -0600

    riko: add discard to ssd backed storage

--- machines/riko/hardware-configuration.nix
+++ machines/riko/hardware-configuration.nix
@@ -16,13 +16,24 @@
   fileSystems."/boot/EFI" = {
     device = "/dev/disk/by-label/efi";
     fsType = "vfat";
-    options = [ "defaults" "x-gvfs-hide" ];
+    options = [
+      "defaults"
+      "x-gvfs-hide"
+      "noatime"
+      "nodiratime"
+    ];
   };
 
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
-    options = [ "defaults" "x-gvfs-hide" ];
+    options = [
+      "defaults"
+      "x-gvfs-hide"
+      "noatime"
+      "nodiratime"
+      "discard"
+    ];
   };
 
   boot.resumeDevice = "/dev/disk/by-label/nixos";
