17557cf8fd3c51c716e2196ee9dd4e0821856573
Lucas Riutzel <lriutzel@gmail.com>
2022-01-04 00:30:18 -0600

    Update to Mikrotik Winbox version 3.32

--- packages/winbox.nix
+++ packages/winbox.nix
@@ -1,8 +1,8 @@
 {pkgs}:
 let
   source = builtins.fetchurl {
-    url = "https://download.mikrotik.com/winbox/3.31/winbox.exe";
-    sha256 = "cb22a2539c49969fd541562e5fbf6974212f7badf2577494ce2fbf73dd798007";
+    url = "https://download.mikrotik.com/winbox/3.32/winbox.exe";
+    sha256 = "07322be03e3b272d9af2cde87d6bb6baa001b9e30413e8740a6af178b65d35a3";
   };
   bin = pkgs.wrapWine {
     name = "winbox";
@@ -25,5 +25,6 @@ in pkgs.symlinkJoin {
   name = "winbox";
   paths = [bin desktop];
 }
+# @TODO figure out how to add dark themes into wine
 # dark theme https://gist.github.com/Zeinok/ceaf6ff204792dde0ae31e0199d89398
 # plus wine-breeze-dark.reg
