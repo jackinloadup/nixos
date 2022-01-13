900fcee41ad41de0d5fcc022948a558501ad693d
Lucas Riutzel <lriutzel@gmail.com>
2022-01-13 14:52:34 -0600

    move systemwide font config to its own file

--- modules/machine/default.nix
+++ modules/machine/default.nix
@@ -7,6 +7,7 @@ in {
   imports = [
     ./chirp.nix
     ./encryptedRoot.nix
+    ./fonts.nix
     ./quietBoot.nix
     ./lowLevelXF86keys.nix
     ./adb.nix
@@ -106,50 +107,6 @@ in {
       ];
     };
 #  } // (if cfg.sizeTarget > -1 then {
-
-    fonts = mkIf (cfg.sizeTarget > 0) {
-      fontconfig = with settings.theme.font; {
-        enable = true;
-        antialias = true;
-        defaultFonts = {
-          serif = [ serif.family ];
-          sansSerif = [ normal.family ];
-          monospace = [ mono.family ];
-          emoji = [ emoji.family ];
-        };
-
-        localConf = ''
-          <?xml version="1.0"?>
-          <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-          <fontconfig>
-            <alias binding="weak">
-              <family>monospace</family>
-              <prefer>
-                <family>emoji</family>
-              </prefer>
-            </alias>
-            <alias binding="weak">
-              <family>sans-serif</family>
-              <prefer>
-                <family>emoji</family>
-              </prefer>
-            </alias>
-            <alias binding="weak">
-              <family>serif</family>
-              <prefer>
-                <family>emoji</family>
-              </prefer>
-            </alias>
-          </fontconfig>
-        '';
-      };
-      fonts = with pkgs; [
-        # Nerdfonts is kinda heavy. We are cutting it fown but still looks like it might be 4-10mb
-        (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" ]; })
-        lato
-        noto-fonts-emoji
-      ];
-    };
 
     # List packages installed in system profile. To search, run:
     # $ nix search wget
