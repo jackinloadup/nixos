a2c6d2ac6255f22b1f48d9204b2d2ba648d38f7f
Lucas Riutzel <lriutzel@gmail.com>
2022-01-20 20:07:35 -0600

    remove i3 from gaming module

--- modules/gaming/default.nix
+++ modules/gaming/default.nix
@@ -20,15 +20,7 @@ in {
       xorg.xhost # for `xhost si:localuser:root`
     ];
     services.xserver = {
-      enable = true;
-      autorun = false;
-      displayManager.defaultSession = "none+i3";
       modules = [ pkgs.xlibs.xf86inputjoystick ];
-
-      windowManager.i3 = {
-        enable = true;
-        package = pkgs.i3-gaps;
-      };
     };
     qt5.enable = true;
     qt5.platformTheme = "gtk2";
