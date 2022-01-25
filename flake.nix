5fff56a45fc8718e19745f79185ee7e696484e23
Lucas Riutzel <lriutzel@gmail.com>
2022-01-25 10:37:09 -0600

    allowUnfree shouldn't be here and isn't needed.

--- flake.nix
+++ flake.nix
@@ -92,9 +92,6 @@
           ];
         };
     in {
-      # Allow unfree packages.
-      nixpkgs.config.allowUnfree = true;
-
       # Expose overlay to flake outputs, to allow using it from other flakes.
       # Flake inputs are passed to the overlay so that the packages defined in
       # it can use the sources pinned in flake.lock
