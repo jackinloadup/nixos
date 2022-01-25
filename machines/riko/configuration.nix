1e98493bb0771b040288760b954d19cda84f8aa0
Lucas Riutzel <lriutzel@gmail.com>
2022-01-25 12:58:15 -0600

    dim or brighten display when plugged in or not.

--- machines/riko/configuration.nix
+++ machines/riko/configuration.nix
@@ -60,6 +60,32 @@ in {
 ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTR{idProduct}=="c52b", TEST=="power/control", ATTR{power/control}:="auto", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}:="60000"
   '';
 
+  hardware.brillo.enable = true;
+  services.acpid = {
+    enable = true;
+    handlers = {
+      ac-power = {
+        action = ''
+          vals=($1)  # space separated string to array of multiple values
+          case ''${vals[3]} in
+              00000000)
+                  echo unplugged >> /tmp/acpi.log
+                  ${pkgs.brillo}/bin/brillo -e -S 50
+                  ;;
+              00000001)
+                  echo plugged in >> /tmp/acpi.log
+                  ${pkgs.brillo}/bin/brillo -e -S 100
+                  ;;
+              *)
+                  echo unknown >> /tmp/acpi.log
+                  ;;
+          esac
+        '';
+        event = "ac_adapter/*";
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
