86065ac3bb1c04f6d39fea0cbba3501ebb1747c5
Lucas Riutzel <lriutzel@gmail.com>
2022-01-27 17:32:24 -0600

    fix tuigreeter not having sessions

--- modules/machine/greetd.nix
+++ modules/machine/greetd.nix
@@ -1,7 +1,10 @@
 { lib, pkgs, config, ... }:
 
 with lib;
-{
+let
+  desktops = config.services.xserver.displayManager.sessionData.desktops;
+  sessions = "${desktops}/share/wayland-sessions:${desktops}/share/xsessions";
+in {
   imports = [ ];
 
   options.machine.displayManager = mkOption {
@@ -13,7 +16,7 @@ with lib;
       enable = true;
       settings = {
         default_session = {
-          command = "${lib.makeBinPath [pkgs.greetd.tuigreet] }/tuigreet --time --cmd sway";
+          command = "${lib.makeBinPath [pkgs.greetd.tuigreet] }/tuigreet --sessions ${sessions} --time --cmd sway ";
           user = "lriutzel";
         };
         initial_session = {
