63574eac3c794dfd9a8334342aa49c149a47e89a
Lucas Riutzel <lriutzel@gmail.com>
2022-01-28 19:27:14 -0600

    don't allow systemd to write over tuigreet

--- modules/machine/greetd.nix
+++ modules/machine/greetd.nix
@@ -12,12 +12,16 @@ in {
   };
 
   config = mkIf (config.machine.displayManager == "greetd") {
+    # keep systemd startup logs on tty1
+    boot.kernelParams = [ "console=tty1" ];
+
     services.greetd = {
       enable = true;
+      vt = 2; # use tty2 to stay away from systemd startup logs
       settings = {
         default_session = {
-          command = "${lib.makeBinPath [pkgs.greetd.tuigreet] }/tuigreet --sessions ${sessions} --time --cmd sway ";
-          user = "lriutzel";
+          command = "${lib.makeBinPath [pkgs.greetd.tuigreet] }/tuigreet -i --sessions ${sessions} --time ";
+          user = "greeter";
         };
         initial_session = {
           command = "sway";
