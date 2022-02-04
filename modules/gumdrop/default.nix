04f6bd8559277714dd7ad6449e2ef5a5642d1400
Lucas Riutzel <lriutzel@gmail.com>
2022-02-04 02:26:48 +0000

    add more verbose comments on nfs mount

--- modules/gumdrop/default.nix
+++ modules/gumdrop/default.nix
@@ -41,13 +41,15 @@ in {
     })
 
     (mkIf config.gumdrop.storageServer {
+      # https://nixos.wiki/wiki/NFS
       fileSystems."/gumdrop" = {
         device = "freenas.home.lucasr.com:/mnt/storage";
         fsType = "nfs";
         options = [
-          "noauto"
-          "x-systemd.automount"
+          "noauto" # lazy mounting
+          "x-systemd.automount" # lazy mounting
           "x-systemd.idle-timeout=600" # disconnects after 10 minutes (i.e. 600 seconds)
+          #"nfsvers=4.2" # not sure if needed but we can specify the specific version
         ];
       };
     })
