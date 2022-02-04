1a44d0a06dc30aa43674739a41bdf320e26134ec
Lucas Riutzel <lriutzel@gmail.com>
2022-02-04 02:25:12 +0000

    reduce standard packges included with gnome

--- modules/machine/gnome.nix
+++ modules/machine/gnome.nix
@@ -14,9 +14,32 @@ with lib;
     # gnome has its own power management tool
     services.tlp.enable = mkForce false;
 
+    # add if icon theme isn't a thing yet
+    environment.systemPackages = with pkgs; [
+      gnome.adwaita-icon-theme
+      gnome.gnome-session
+    ];
+
     services.xserver.displayManager.job.logToJournal = true;
     services.xserver.desktopManager.gnome.enable = true;
+
+    environment.gnome.excludePackages = with pkgs; [
+      gnome.cheese
+      gnome-photos
+      gnome.gnome-music
+      gnome.gnome-terminal
+      gnome.gedit
+      epiphany
+      evince
+      gnome.gnome-characters
+      gnome.totem
+      gnome.tali
+      gnome.iagno
+      gnome.hitori
+      gnome.atomix
+      gnome-tour
+      gnome.geary
+    ];
   };
 }
-
 
