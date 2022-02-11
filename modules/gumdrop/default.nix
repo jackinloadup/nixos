1df1e699cc4421b39ab4186cbbb894cb06efd49b
Lucas Riutzel <lriutzel@gmail.com>
2022-02-11 02:09:33 +0000

    break apart gumdrop module

--- modules/gumdrop/default.nix
+++ modules/gumdrop/default.nix
@@ -4,54 +4,14 @@ let
   cfg = config.machine;
   settings = import ../../settings;
 in {
-  imports = [];
-
-  options.gumdrop = {
-    printerScanner = mkEnableOption "Setup printer scanner";
-    storageServer = mkEnableOption "Setup connection to storage server";
-  };
-
-  config = mkMerge [
-    (mkIf config.gumdrop.printerScanner {
-      # may not be nessisary if multiple dhcp search/domain things stack 
-      # as the machine is connected to more networks
-      networking.search = [ "home.lucasr.com" ];
-      environment.systemPackages = with pkgs; mkIf (cfg.sizeTarget > 0) [
-        gnome.simple-scan
-      ];
-      # Enable CUPS to print documents.
-      services.printing.enable = true;
-      services.printing.drivers = with pkgs; [ brlaser cups-filters ];
-
-      # Enable Sane to scan documents.
-      hardware.sane.enable = true;
-      hardware.sane.brscan4.enable = true;
-      hardware.sane.brscan4.netDevices = {
-        "Home" = {
-          "ip" = "10.16.1.64";
-          "model" = "MFC-9130CW";
-        };
-      };
-
-      users.users = with settings.user; {
-        ${username} = {
-          extraGroups = [ "scanner" "lp" ];
-        };
-      };
-    })
-
-    (mkIf config.gumdrop.storageServer {
-      # https://nixos.wiki/wiki/NFS
-      fileSystems."/gumdrop" = {
-        device = "freenas.home.lucasr.com:/mnt/storage";
-        fsType = "nfs";
-        options = [
-          "noauto" # lazy mounting
-          "x-systemd.automount" # lazy mounting
-          "x-systemd.idle-timeout=600" # disconnects after 10 minutes (i.e. 600 seconds)
-          #"nfsvers=4.2" # not sure if needed but we can specify the specific version
-        ];
-      };
-    })
+  imports = [
+    ./printer-scanner.nix
+    ./storage-server.nix
   ];
+
+  config = {
+    # may not be nessisary if multiple dhcp search/domain things stack
+    # as the machine is connected to more networks
+    networking.search = [ "home.lucasr.com" ];
+  };
 }
