13f51e10d49400526b2ced0c1d4463df4bc93d5b
Lucas Riutzel <lriutzel@gmail.com>
2022-03-16 16:07:12 -0500

    inital module which contains many 'hacker' tools

---
+++ modules/arewehackersyet/default.nix
@@ -0,0 +1,54 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let
+  cfg = config.machine;
+  settings = import ../../settings;
+in {
+  imports = [];
+
+  options.machine = {
+    # https://jjjollyjim.github.io/arewehackersyet/index.html
+    arewehackersyet = mkEnableOption "Tools from kali linux" ;
+  };
+
+  config = mkIf cfg.arewehackersyet {
+      environment.systemPackages = with pkgs; mkIf (cfg.sizeTarget > 0) [
+        gnome.simple-scan
+       	aircrack-ng 
+        wifite2
+        bully
+        cowpatty
+        hashcat
+        kismet
+        macchanger
+        pixiewps
+        ccrypt
+        steghide
+        mdbtools
+        sqlitebrowser
+        beef
+        metasploit
+        apktool
+        exiv2
+        ext4magic
+        extundelete
+        yara
+        sslsplit
+        sslscan
+        sslh
+        ssldump
+        wpscan
+        zap
+        siege
+        redsocks
+        proxytunnels
+        proxychains
+        nmap
+        httrack
+        thc-hydra
+        dirb
+        cadaver
+        burpsuite
+      ];
+  };
+}
