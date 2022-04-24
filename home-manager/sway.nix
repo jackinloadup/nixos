6d8da8d4362e0bcd75dabc55190f622ba9836a27
Lucas Riutzel <lriutzel@gmail.com>
2022-04-23 20:44:45 -0500

    refactor sway config

--- home-manager/sway.nix
+++ home-manager/sway.nix
@@ -13,6 +13,7 @@ let
       # thus if sway crashes the resulting terminal will not be logged in
       #[[ "$(tty)" == /dev/tty1 ]] && exec sway
     '';
+  swayConfig = config.wayland.windowManager.sway.config;
 in
 {
   imports = [
@@ -151,104 +152,101 @@ in
 
       menu = "${pkgs.j4-dmenu-desktop}/bin/j4-dmenu-desktop --no-generic --term=foot --dmenu='bemenu -i -l 10'" ;
 
-      keybindings =
-        let
-          mod = config.wayland.windowManager.sway.config.modifier;
-          inherit (config.wayland.windowManager.sway.config)
-            left down up right menu terminal;
-        in
-        {
-          "${mod}+Return" = "exec ${terminal}";
+      keybindings = let
+        inherit (swayConfig) left down up right menu terminal modifier;
+        mod = modifier;
+      in
+      {
+        "${mod}+Return" = "exec ${terminal}";
 
-          "${mod}+Shift+q" = "kill";
-          "${mod}+space" = "exec ${menu}";
+        "${mod}+Shift+q" = "kill";
+        "${mod}+space" = "exec ${menu}";
 
-          "${mod}+${left}" = "focus left";
-          "${mod}+${down}" = "focus down";
-          "${mod}+${up}" = "focus up";
-          "${mod}+${right}" = "focus right";
+        "${mod}+${left}" = "focus left";
+        "${mod}+${down}" = "focus down";
+        "${mod}+${up}" = "focus up";
+        "${mod}+${right}" = "focus right";
 
-          "${mod}+Left" = "focus left";
-          "${mod}+Down" = "focus down";
-          "${mod}+Up" = "focus up";
-          "${mod}+Right" = "focus right";
+        "${mod}+Left" = "focus left";
+        "${mod}+Down" = "focus down";
+        "${mod}+Up" = "focus up";
+        "${mod}+Right" = "focus right";
 
-          "${mod}+Shift+${left}" = "move left";
-          "${mod}+Shift+${down}" = "move down";
-          "${mod}+Shift+${up}" = "move up";
-          "${mod}+Shift+${right}" = "move right";
+        "${mod}+Shift+${left}" = "move left";
+        "${mod}+Shift+${down}" = "move down";
+        "${mod}+Shift+${up}" = "move up";
+        "${mod}+Shift+${right}" = "move right";
 
-          "${mod}+Shift+Left" = "move workspace to output left";
-          "${mod}+Shift+Up" = "move workspace to output up";
-          "${mod}+Shift+Down" = "move workspace to output down";
-          "${mod}+Shift+Right" = "move workspace to output right";
+        "${mod}+Shift+Left" = "move workspace to output left";
+        "${mod}+Shift+Up" = "move workspace to output up";
+        "${mod}+Shift+Down" = "move workspace to output down";
+        "${mod}+Shift+Right" = "move workspace to output right";
 
-          "${mod}+Shift+space" = "floating toggle";
-          #"${mod}+space" = "focus mode_toggle";
+        "${mod}+Shift+space" = "floating toggle";
+        #"${mod}+space" = "focus mode_toggle";
 
-          "${mod}+1" = "workspace number 1";
-          "${mod}+2" = "workspace number 2";
-          "${mod}+3" = "workspace number 3";
-          "${mod}+4" = "workspace number 4";
-          "${mod}+5" = "workspace number 5";
-          "${mod}+6" = "workspace number 6";
-          "${mod}+7" = "workspace number 7";
-          "${mod}+8" = "workspace number 8";
-          "${mod}+9" = "workspace number 9";
-          "${mod}+0" = "workspace number 10";
+        "${mod}+1" = "workspace number 1";
+        "${mod}+2" = "workspace number 2";
+        "${mod}+3" = "workspace number 3";
+        "${mod}+4" = "workspace number 4";
+        "${mod}+5" = "workspace number 5";
+        "${mod}+6" = "workspace number 6";
+        "${mod}+7" = "workspace number 7";
+        "${mod}+8" = "workspace number 8";
+        "${mod}+9" = "workspace number 9";
+        "${mod}+0" = "workspace number 10";
 
-          "${mod}+Shift+1" = "move container to workspace number 1";
-          "${mod}+Shift+2" = "move container to workspace number 2";
-          "${mod}+Shift+3" = "move container to workspace number 3";
-          "${mod}+Shift+4" = "move container to workspace number 4";
-          "${mod}+Shift+5" = "move container to workspace number 5";
-          "${mod}+Shift+6" = "move container to workspace number 6";
-          "${mod}+Shift+7" = "move container to workspace number 7";
-          "${mod}+Shift+8" = "move container to workspace number 8";
-          "${mod}+Shift+9" = "move container to workspace number 9";
-          "${mod}+Shift+0" = "move container to workspace number 10";
+        "${mod}+Shift+1" = "move container to workspace number 1";
+        "${mod}+Shift+2" = "move container to workspace number 2";
+        "${mod}+Shift+3" = "move container to workspace number 3";
+        "${mod}+Shift+4" = "move container to workspace number 4";
+        "${mod}+Shift+5" = "move container to workspace number 5";
+        "${mod}+Shift+6" = "move container to workspace number 6";
+        "${mod}+Shift+7" = "move container to workspace number 7";
+        "${mod}+Shift+8" = "move container to workspace number 8";
+        "${mod}+Shift+9" = "move container to workspace number 9";
+        "${mod}+Shift+0" = "move container to workspace number 10";
 
-          "${mod}+p" =
-            "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g- screenshot-$(date +%Y%m%d-%H%M).png";
+        "${mod}+p" =
+          "exec ${pkgs.slurp}/bin/slurp | ${pkgs.grim}/bin/grim -g- screenshot-$(date +%Y%m%d-%H%M).png";
 
-          "${mod}+g" = "split h";
-          "${mod}+v" = "split v";
-          "${mod}+f" = "fullscreen toggle";
-          "${mod}+comma" = "layout stacking";
-          "${mod}+period" = "layout tabbed";
-          "${mod}+slash" = "layout toggle split";
-          "${mod}+a" = "focus parent";
-          "${mod}+s" = "focus child";
+        "${mod}+g" = "split h";
+        "${mod}+v" = "split v";
+        "${mod}+f" = "fullscreen toggle";
+        "${mod}+comma" = "layout stacking";
+        "${mod}+period" = "layout tabbed";
+        "${mod}+slash" = "layout toggle split";
+        "${mod}+a" = "focus parent";
+        "${mod}+s" = "focus child";
 
-          "${mod}+Shift+c" = "reload";
-          "${mod}+Shift+r" = "restart";
-          "${mod}+Shift+v" = ''mode "system:  [s]oft reboot [r]eboot  [p]oweroff  [l]ogout"'';
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+        "${mod}+Shift+v" = ''mode "system:  [s]oft reboot [r]eboot  [p]oweroff  [l]ogout"'';
 
-          "${mod}+r" = "mode resize";
+        "${mod}+r" = "mode resize";
 
-          #"${mod}+L" = ''exec ${pkgs.swaylock}/bin/swaylock -i "$(${wallpaper}/bin/wallpaper get)"'';
-          "${mod}+Shift+Delete" = ''exec ${pkgs.swaylock}/bin/swaylock -i "~/background.jpg"'';
-          #"${mod}+k" = "exec ${pkgs.mako}/bin/makoctl dismiss";
-          #"${mod}+Shift+k" = "exec ${pkgs.mako}/bin/makoctl dismiss -a";
+        #"${mod}+L" = ''exec ${pkgs.swaylock}/bin/swaylock -i "$(${wallpaper}/bin/wallpaper get)"'';
+        "${mod}+Shift+Delete" = ''exec ${pkgs.swaylock}/bin/swaylock -i "~/background.jpg"'';
+        #"${mod}+k" = "exec ${pkgs.mako}/bin/makoctl dismiss";
+        #"${mod}+Shift+k" = "exec ${pkgs.mako}/bin/makoctl dismiss -a";
 
-          #"XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
-          #"XF86AudioRaiseVolume" =
-          #  "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
-          #"XF86AudioLowerVolume" =
-          #  "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
-          #"${mod}+z" = "exec ${pkgs.zathura}/bin/zathura";
+        #"XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+        #"XF86AudioRaiseVolume" =
+        #  "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
+        #"XF86AudioLowerVolume" =
+        #  "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
+        #"${mod}+z" = "exec ${pkgs.zathura}/bin/zathura";
 
-          "${mod}+Shift+minus" = "move container to scratchpad";
-          "${mod}+minus" = "scratchpad show";
-        } // (if hostName == "spica" then { # smart. use hostname then append based on that
-          "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
-          "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
-        } else { });
+        "${mod}+Shift+minus" = "move container to scratchpad";
+        "${mod}+minus" = "scratchpad show";
+      } // (if hostName == "spica" then { # smart. use hostname then append based on that
+        "XF86MonBrightnessUp" = "exec ${pkgs.light}/bin/light -A 10";
+        "XF86MonBrightnessDown" = "exec ${pkgs.light}/bin/light -U 10";
+      } else { });
 
-        modes =
-        let
-          terminal = config.wayland.windowManager.sway.config.terminal;
-        in {
+      modes = let
+        terminal = swayConfig.terminal;
+      in {
         "system:  [s]oft reboot [r]eboot  [p]oweroff  [l]ogout" = {
           s = "exec ${terminal} -e ./kexec-systemd.sh";
           r = "exec reboot";
@@ -257,17 +255,20 @@ in
           Return = "mode default";
           Escape = "mode default";
         };
-        resize = {
+
+        resize = let
           Left = "resize shrink width 10 px or 10 ppt";
-          Down = "resize grow height 10 px or 10 ppt";
-          Up = "resize shrink height 10 px or 10 ppt";
           Right = "resize grow width 10 px or 10 ppt";
-          h = "resize shrink width 10 px or 10 ppt";
-          j = "resize grow height 10 px or 10 ppt";
-          k = "resize shrink height 10 px or 10 ppt";
-          l = "resize grow width 10 px or 10 ppt";
-          Return = "mode default";
+          Up = "resize shrink height 10 px or 10 ppt";
+          Down = "resize grow height 10 px or 10 ppt";
           Escape = "mode default";
+        in {
+          inherit Left Right Up Down Escape;
+          h = Left;
+          j = Down;
+          k = Up;
+          l = Right;
+          Return = Escape;
         };
       };
 
