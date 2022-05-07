2c83d2daae455259883f86cf3cdf3f63cb9c9c02
Lucas Riutzel <lriutzel@gmail.com>
2022-05-07 11:42:15 -0500

    Remove julia machine

--- machines/julia/modules.nix
+++
@@ -1,7 +0,0 @@
-{nixos-hardware, ...}:
-[
-    nixos-hardware.nixosModules.common-pc-laptop
-    nixos-hardware.nixosModules.common-pc-laptop-acpi_call
-    nixos-hardware.nixosModules.common-pc-ssd
-    nixos-hardware.nixosModules.common-cpu-intel
-]
