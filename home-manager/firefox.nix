6be0357b1dbff3388ecb203a08899267fc2c7c1e
Lucas Riutzel <lriutzel@gmail.com>
2022-05-27 22:44:22 -0500

    firefox cleanup

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -1,24 +1,27 @@
 { config, pkgs, nixosConfig, lib, inputs, ... }:
+let
+  inherit (lib.attrsets) genAttrs;
+in
 {
   home.packages = with pkgs; lib.mkIf (nixosConfig.machine.sizeTarget > 1 ) [
     ffmpeg # used for firefox va-api accel with media.rdd-ffmpeg
   ];
 
   xdg.mimeApps = {
-    defaultApplications = {
-      "application/x-extension-htm" = "firefox.desktop";
-      "application/x-extension-html" = "firefox.desktop";
-      "application/x-extension-shtml" = "firefox.desktop";
-      "application/x-extension-xht" = "firefox.desktop";
-      "application/x-extension-xhtml" = "firefox.desktop";
-      "application/xhtml+xml" = "firefox.desktop";
-      "text/html" = "firefox.desktop";
-      "x-scheme-handler/chrome" = "firefox.desktop";
-      "x-scheme-handler/http" = "firefox.desktop";
-      "x-scheme-handler/https" = "firefox.desktop";
-      "x-scheme-handler/about" = "firefox.desktop";
-      "x-scheme-handler/unknown" = "firefox.desktop";
-    };
+    defaultApplications = genAttrs [
+      "application/x-extension-htm"
+      "application/x-extension-html"
+      "application/x-extension-shtml"
+      "application/x-extension-xht"
+      "application/x-extension-xhtml"
+      "application/xhtml+xml"
+      "text/html"
+      "x-scheme-handler/chrome"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/about"
+      "x-scheme-handler/unknown"
+    ] (name: "firefox.desktop");
   };
 
   programs.firefox = {
