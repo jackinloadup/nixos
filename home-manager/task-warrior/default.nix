0eabdebe5a09f7a1091144db5e7ad2de300ae577
Lucas Riutzel <lriutzel@gmail.com>
2022-07-16 07:04:57 -0500

    move task warrior into its own file

--- home-manager/task-warrior/default.nix
+++ home-manager/task-warrior/default.nix
@@ -12,4 +12,14 @@
     taskwarrior-tui
   ];
 
+  programs.taskwarrior = {
+    enable = true;
+    colorTheme = "dark-blue-256";
+    config = {
+      confirmation = false;
+      report.minimal.filter = "status:pending";
+      report.active.columns = [ "id" "start" "entry.age" "priority" "project" "due" "description" ];
+      report.active.labels = [ "ID" "Started" "Age" "Priority" "Project" "Due" "Description" ];
+    };
+  };
 }
