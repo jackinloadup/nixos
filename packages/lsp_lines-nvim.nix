c8bc4a49410b364f73e335a126000a6e3cf3f204
Lucas Riutzel <lriutzel@gmail.com>
2022-07-26 20:09:56 -0500

    add custom version of lsp_lines

--- packages/lsp_lines-nvim.nix
+++ packages/lsp_lines-nvim.nix
@@ -1,12 +1,15 @@
-{pkgs, system}:
+{pkgs}:
 
-buildVimPluginFrom2Nix {
-    pname = "lsp_lines.nvim";
-    version = "2022-06-29";
-    meta.homepage = "https://git.sr.ht/~whynothugo/lsp_lines.nvim";
-    src = fetchgit {
-        url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim";
-        rev = "3b57922d2d79762e6baedaf9d66d8ba71f822816";
-        #sha256 = "12i9v3vnbl0djx43y46xli3f5nbf2yly4c7d0mcq8682yxfq149b";
-    };
+let
+  buildVimPluginFrom2Nix = pkgs.vimUtils.buildVimPluginFrom2Nix;
+  fetchgit = pkgs.fetchgit;
+in buildVimPluginFrom2Nix {
+  pname = "lsp_lines.nvim";
+  version = "2022-06-29";
+  meta.homepage = "https://git.sr.ht/~whynothugo/lsp_lines.nvim";
+  src = fetchgit {
+      url = "https://git.sr.ht/~whynothugo/lsp_lines.nvim";
+      rev = "3b57922d2d79762e6baedaf9d66d8ba71f822816";
+      sha256 = "1vHMs2Nej/uTancRbo5SNuovE+hxw9fR20pVVfH9UIs=";
   };
+}
