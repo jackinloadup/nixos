44c51007bf39d5427443c3a3007022f3d3cfe437
Lucas Riutzel <lriutzel@gmail.com>
2022-07-26 20:08:31 -0500

    neovim got snippet engine to support completion and more completion refinement

--- home-manager/neovim/nvim-cmp.nix
+++ home-manager/neovim/nvim-cmp.nix
@@ -5,11 +5,58 @@
   local cmp = require("cmp")
 
   cmp.setup({
+    mapping = {
+      ["<C-n>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert },
+      ["<C-p>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert },
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-e>"] = cmp.mapping.abort(),
+      ["<c-y>"] = cmp.mapping(
+        cmp.mapping.confirm {
+          behavior = cmp.ConfirmBehavior.Insert,
+          select = true,
+        },
+        { "i", "c" }
+      ),
+
+      ["<c-space>"] = cmp.mapping {
+        i = cmp.mapping.complete(),
+        c = function(
+          _ --[[fallback]]
+        )
+          if cmp.visible() then
+            if not cmp.confirm { select = true } then
+              return
+            end
+          else
+            cmp.complete()
+          end
+        end,
+      },
+    },
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
-      { name = "path" },
-      { name = "buffer" },
-    })
+      { name = "luasnip" },
+      { name = "buffer", keyword_length = 5 },
+      { name = "path" }
+    }),
+    snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    },
+    formatting = {
+      format = require("lspkind").cmp_format {
+        with_text = true,
+        menu = {
+          buffer = "[buf]",
+          nvim_lsp = "[LSP]",
+          nvim_lua = "[api]",
+          path = "[path]",
+          luasnip = "[snip]",
+        },
+      },
+    },
   })
   '';
 }
