6ff713065c2812ef3ab005aad49514c5e1a9f8ce
Lucas Riutzel <lriutzel@gmail.com>
2022-09-14 23:59:53 -0500

    Don't include all home-manager modules by default
    moved many of those modules into lriutzel user

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -4,56 +4,16 @@ with inputs;
 let
   settings = import ../settings;
   ifGraphical = if (nixosConfig.machine.sizeTarget > 1) then true else false;
-in
-{
-  imports = [
-    (import "${inputs.impermanence}/home-manager.nix")
-    ./alacritty.nix
-    base16.hmModule
-    ./development.nix
-    ./dunst.nix
-    ./firefox.nix
-    ./foot.nix
-    ./i3.nix
-    ./mpv.nix
-    ./neovim/default.nix
-    ./sway/default.nix
-    ./impermanence.nix
-    #./task-warrior
-    #./zoom.nix
-  ];
+in {
+  imports = [ ];
 
   config = {
-    nixpkgs.config.allowUnfree = true;
-    nixpkgs.overlays = [
-      self.overlays.default
-      nur.overlay
-    ];
-
-    nix.package = pkgs.nix;
-    nix.extraOptions = ''
-      experimental-features = nix-command flakes
-      auto-optimise-store = true
-    '';
-
-    themes.base16 = with settings.theme; {
-      enable = true;
-      scheme = base16.scheme;
-      variant = base16.variant;
-      defaultTemplateType = "default";
-      # Add extra variables for inclusion in custom templates
-      extraParams = {
-        fontName = font.mono.family;
-        fontSize = font.size;
-      };
-    };
-
     xdg = {
-      enable = true;
-      userDirs.enable = true;
+      enable = mkDefault true;
+      userDirs.enable = mkDefault true;
 
       mimeApps = {
-        enable = true;
+        enable = mkDefault ifGraphical;
         defaultApplications = {
           "application/pdf" = "org.pwmt.zathura.desktop";
 
@@ -69,7 +29,7 @@ in
     };
 
     xsession = {
-      enable = true;
+      enable = mkDefault true;
     };
 
     home.keyboard = null; # only works with x11 i believe
@@ -77,88 +37,32 @@ in
         package = pkgs.quintom-cursor-theme;
         name = "Quintom_Ink";
         size = 32;
-        gtk.enable = true;
-        x11.enable = true;
+        gtk.enable = mkDefault ifGraphical;
+        x11.enable = mkDefault ifGraphical;
       };
 
     gtk = with settings.theme; {
-      enable = if (nixosConfig.machine.sizeTarget > 1 ) then true else false;
+      enable = ifGraphical;
       font.name = "${font.normal.family} ${font.normal.style} ${toString font.size}";
       theme.name = gtk.name;
       theme.package = pkgs.${gtk.package};
     };
 
-    programs.bash.enable = true;
-    programs.bash.initExtra = ''
-      source ${config.lib.base16.templateFile { name = "shell"; }}
-      eval "$(starship init bash)"
-    '';
-
-    programs.readline = {
-      enable = true;
-      bindings = {
-        "\\C-h" = "backward-kill-word";
-      };
-      extraConfig = ''
-set editing-mode vi
-
-set show-mode-in-prompt on
-set vi-ins-mode-string "\1\e[5 q\2"
-set vi-cmd-mode-string "\1\e[2 q\2"
-
-set keymap vi-command
-# j and k should search for the string of characters preceding the cursor
-"k": history-search-backward
-"j": history-search-forward
-
-set keymap vi-insert
-# inoremap jk <Esc>
-"jk": vi-movement-mode
-      '';
-    };
-
-
     programs.home-manager.enable = true;
 
 
-    # This value determines the Home Manager release that your
-    # configuration is compatible with. This helps avoid breakage
-    # when a new Home Manager release introduces backwards
-    # incompatible changes.
-    #
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
-    home.stateVersion = "21.05";
-
     home.sessionVariables = {
-      TERMINFO_DIRS="${config.home.homeDirectory}/.nix-profile/share/terminfo";
-      WSLENV="TERMINFO_DIRS";
       XAUTHORITY="${config.home.homeDirectory}/.Xauthority";
-      NIXOS_CONFIG="${config.home.homeDirectory}/dotfiles/flake.nix";
-      NIX_PATH="nixos-config=/home/lriutzel/dotfiles/flake.nix:$NIX_PATH";
     };
 
-    home.username = settings.user.username;
-    home.homeDirectory = lib.mkOverride 10 "/home/${settings.user.username}";
-
-    home.packages = with pkgs; (if (nixosConfig.machine.sizeTarget > 0 ) then [
-      iotop
-      inetutils
-      usbutils # an alternative could be busybox cope toybox
-      unzip # duh
-      lftp
-
-      units
-    ] else []) ++ (if (nixosConfig.machine.sizeTarget > 1 ) then [
+    home.packages = with pkgs; []
+      ++ lib.optionals ifGraphical [
       #(aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
 
       # GUI
       # ---------------------------------------
-      zathura # PDF / Document viewer
       libreoffice # Office suite
       libnotify # for notify-send
-      imv # minimal image viewer
       # darktile # alternative PTY to try out. GPU + go
 
       # Spelling
@@ -166,109 +70,20 @@ set keymap vi-insert
       hunspellDicts.en_US-large
       hyphen
 
-      #freeoffice # office suite UNFREE
-      fractal # matrix client
-      nheko   # matrix client
-      thunderbird # Email client
-      tixati # bittorrent client
-      #mumble # voice chat application
-      #nur.repos.arc.packages.mumble_1_4
-      signal-desktop # messaging client
       chromium # web browser
       xfce.thunar # File manager
       #pantheon.elementary-files
-      cinnamon.nemo
-      speedcrunch # calculator 
-      nota # fancy cli calculator
+      cinnamon.nemo # File manager
       #flameshot
-      gnome.vinagre
       gnome.gnome-disk-utility
       #calibre # ebook tool,  assist with kindle
       #obsidian # Markdown information archive
-      unar # An archive unpacker program GUI & TUI
-
-      exodus # Cryptowallet
-      electron-cash # BCH walle
-
-      tor-browser-bundle-bin
-
-      python39Packages.xdot # graphviz viewer
-      graphviz
-      blender
-
-      ## Video
-      handbrake
-      lbry
-      obs-studio
-      obs-studio-plugins.wlrobs
-      obs-studio-plugins.obs-multi-rtmp
 
       ## Audio
       pavucontrol # GUI volume source/sink manager
-      pulsemixer # TUI volume source/sink manager
-      # Spotify opensource utils?
-      spotify-tui # spotifyd ui
-      spotifyd # music player no ui
-      # NonFree
-      spotify
+    ];
 
-      cawbird # twitter client
-
-      ## Debugging
-      wireshark
-      gparted
-
-      ## Task/notes
-      mindforger
-
-      ## Wine Apps
-      wineApps.winbox
-      #winetricks
-      #wineWowPackages.stable
-
-      # TUI
-      # ----------------
-      jless # json viewer
-      tealdeer # $tldr strace
-      #bitwarden-cli
-      python39Packages.youtube-dl # there is an alt youtube-dl-lite
-      xdg-utils # for xdg-open
-      #nur.repos.ambroisie.comma # like nix-shell but more convinient
-      nix-index
-      nmap
-      nixos-shell
-
-      ## spreadsheet stuffs
-      sc-im
-      visidata
-
-      # TUI to GUI helpers
-      #dragon-drop # in unstable its maybe xdragon
-
-      ## networking
-      nethogs
-      ngrep
-      fast-cli
-
-      ## Audio
-      playerctl
-
-      # Fun
-      asciiquarium # Fun aquarium animation
-      cmatrix # Fun matrix animation
-    ] else []);
-
-    services = lib.mkIf (nixosConfig.machine.sizeTarget > 1 ) {
-      gpg-agent = {
-        enable = true;
-        enableExtraSocket = false;
-        enableScDaemon = false;
-        enableSshSupport = true;
-        defaultCacheTtl = 30;
-        defaultCacheTtlSsh = 30;
-        maxCacheTtl = 3600; # 1 hour
-        maxCacheTtlSsh = 3600; # 1 hour
-      };
+    services = lib.mkIf ifGraphical {
       playerctld.enable = true;
 
       # Add the network manager to the status bar.
@@ -294,9 +109,5 @@ set keymap vi-insert
         tray = "auto";
       };
     };
-
-    home.file."${config.xdg.configHome}/htop/htoprc".source = ./htoprc;
-
-    ## add config above here
   };
 }
--- users/lriutzel/default.nix
+++ users/lriutzel/default.nix
@@ -3,7 +3,9 @@ with lib;
 let
   cfg = config.machine;
   settings = import ./settings.nix;
+  ifTui = if (cfg.sizeTarget > 0) then true else false;
   ifGraphical = if (cfg.sizeTarget > 1) then true else false;
+  ifFull = if (cfg.sizeTarget > 2) then true else false;
 in {
   imports = [
   ];
@@ -37,52 +39,148 @@ in {
     # DON'T set useGlobalPackages! It's not necessary in newer
     # home-manager versions and does not work with configs using
     # nixpkgs.config`
-    home-manager.users.lriutzel = {
+    home-manager.users.lriutzel = let
+      homeDir = "/home/${settings.username}";
+    in {
       imports = [
-        ../../home-manager
+        ../../home-manager/nix.nix
+      ]
+      ++ lib.optionals ifTui [
+        ../../home-manager/tui.nix
+      ]
+      ++ lib.optionals ifGraphical [
+        ../../home-manager/alacritty.nix
+        ../../home-manager/default.nix
+        ../../home-manager/dunst.nix
+        ../../home-manager/firefox.nix
+        ../../home-manager/gpg.nix
+        ../../home-manager/foot.nix
+        ../../home-manager/i3.nix
+        ../../home-manager/impermanence.nix
+        ../../home-manager/mpv.nix
+        ../../home-manager/neovim/default.nix
+        ../../home-manager/sway/default.nix
+        #./task-warrior
+        #./zoom.nix
+      ]
+      ++ lib.optionals ifFull [
+        ../../home-manager/development.nix
       ];
-      programs.git.extraConfig.safe.directory = "/home/lriutzel/dotfiles";
+
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      home.stateVersion = config.system.stateVersion;
+
+      home.username = settings.username;
+      home.homeDirectory = lib.mkOverride 10 homeDir;
+
+      home.sessionVariables = {
+        NIXOS_CONFIG="$HOME/dotfiles/flake.nix";
+        NIX_PATH="nixos-config=/home/lriutzel/dotfiles/flake.nix:$NIX_PATH";
+      };
+
+      programs.git.extraConfig.safe.directory = "${homeDir}/dotfiles";
+
+      home.packages = with pkgs; []
+      ++ lib.optionals ifGraphical [ # TUI tools but loading if graphical
+        fzf
+        unstable.mqttui # mqtt tui
+
+        # markdown tools
+        mdcat # tui viewer
+        mdp # markdown presentation
+        mdr # tui viewer
+        # mdv # tui viewer not in nixpkgs yet
+      ]
+      ++ lib.optionals ifGraphical [
+        emulsion # mimimal linux image viewer built in rust
+        imv # minimal image viewer
+        #tor-browser-bundle-bin
+        zathura # PDF / Document viewer
+
+        python39Packages.xdot # graphviz viewer
+        graphviz
+
+        # Spotify
+        spotify-tui # spotifyd ui
+        spotifyd # music player no ui
+        # NonFree
+        spotify
+
+        #gnome.vinagre # VNC view another computer
+        fractal # matrix client
+        nheko   # matrix client
+        thunderbird # Email client
+        #mumble # voice chat application
+        signal-desktop # messaging client
+
+        ## Task/notes
+        mindforger
+      ]
+      ++ lib.optionals ifFull [
+        unstable.helvum # pipewire patchbay
+        easyeffects # Audio effects
+
+        # crypto
+        trezorctl
+        trezor_agent
+        trezor-suite
+        #exodus # Cryptowallet
+        #electron-cash # BCH walle
+
+        # Media Management
+        mediaelch
+        #filebot
+        mkvtoolnix
+
+        #freeoffice # office suite UNFREE
+        tixati # bittorrent client
+        #nur.repos.arc.packages.mumble_1_4
+        blender # 3D render
+        cawbird # twitter client
+        speedcrunch # gui calculator
+
+        ## Video
+        handbrake
+        lbry
+        obs-studio
+        obs-studio-plugins.wlrobs
+        obs-studio-plugins.obs-multi-rtmp
+
+        ## Debugging
+        wireshark
+        gparted
+
+        ## Wine Apps
+        wineApps.winbox
+
+        kodi-wayland
+      ];
     };
 
-    programs.wireshark.enable = ifGraphical;
+    programs.wireshark.enable = ifFull;
 
     services.trezord.enable = ifGraphical;
 
     environment.systemPackages = with pkgs; mkIf ifGraphical [
       #nix-plugins # Collection of miscellaneous plugins for the nix expression language
-      emulsion # mimimal linux image viewer built in rust
       #nmap-graphical # support removed due to its python2 dependency
-      fzf
-
       #nur.repos.milahu.aether-server # Peer-to-peer ephemeral public communities
-
-      unstable.mqttui # mqtt tui
-
-      unstable.helvum # pipewire patchbay
-      easyeffects
-      trezorctl
-      trezor_agent
-      trezor-suite
-
-      mediaelch
-      # markdown tools
-      mdcat # tui viewer
-      mdp # markdown presentation
-      mdr # tui viewer
-      # mdv # tui viewer not in nixpkgs yet
     ];
 
     hardware.yubikey.enable = ifGraphical;
 
-    # #TODO figure out how to enable only at user level
-    #programs.gnupg.agent = {
-    #  enable = true;
-    #  enableSSHSupport = true;
-    #};
     modules.browsers.firefox = {
       enable = ifGraphical;
       profileName = "lriutzel";
     };
 
+    # user 1002 can only use tun0
+#    networking.firewall.extraCommands = "
+#iptables -A OUTPUT -o lo -m owner --uid-owner 1002 -j ACCEPT
+#iptables -A OUTPUT -o tun0 -m owner --uid-owner 1002 -j ACCEPT
+#iptables -A OUTPUT -m owner --uid-owner 1002 -j REJECT
+#    ";
   };
 }
