b467de61b6ea133e2459f97ef1e80a5017868363
Lucas Riutzel <lriutzel@gmail.com>
2022-10-23 23:24:09 -0500

    kodi overlay working wip

--- overlays/kodi-wayland.nix
+++ overlays/kodi-wayland.nix
@@ -1,17 +1,24 @@
 inputs:
-let
-  inherit inputs;
-  #cp = f: (super.callPackage f) {};
-in self: super: {
-  kodi-wayland = (self.unstable.kodi-wayland.override {
-    joystickSupport = false;
-    x11Support = false;
-    nfsSupport = false;
-    sambaSupport = false;
-  })
+self: super: {
+  kodi-wayland = ((self.unstable.kodi-wayland
   .overrideAttrs(old: {
     patches = old.patches ++ [
       ../patches/kodi-workaround-blank-screen.patch
+      ../patches/kodi-scancode.patch
     ];
+    #passthru = old.passthru // {
+    #  withPackages = old.passthru.withPackages (kodiPkgs: with kodiPkgs; [
+    #    #  keymap
+    #    visualization-waveform
+    #    #self.unstable.kodiPackages.visualization-waveform
+    #  ]);
+    #};
+  }))
+  .override {
+    joystickSupport = true;
+    gbmSupport = false;
+    nfsSupport = false;
+    sambaSupport = false;
+    x11Support = false;
   });
 }
