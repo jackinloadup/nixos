132defa8321efc9de4ebf9f1832a4aa795ddad25
Lucas Riutzel <lriutzel@gmail.com>
2022-10-23 23:19:37 -0500

    make quiet boot more extensive and better commented

--- modules/machine/quietBoot.nix
+++ modules/machine/quietBoot.nix
@@ -13,15 +13,32 @@ in {
       loader.timeout = 1;
       # Quiet durring boot
       initrd.verbose = false;
-      consoleLogLevel = 0;
+
+      # 0 = EMERG
+      # 1 = ALERT
+      # 2 = CRIT
+      # 3 = ERR
+      # 4 = WARNING - default in the majority of linux distributions.
+      #               This level it’s used to display warnings or messages about non imminent errors
+      # 5 = NOTICE
+      # 6 = INFO
+      # 7 = DEBUG
+      consoleLogLevel = 4;
+
+      # Parameters prefixed with "rd." will be read when systemd-udevd is used in an initrd,
+      # those without will be processed both in the initrd and on the host.
       kernelParams = [
         "quiet"
-        "vga=current"
-        "systemd.show_status=auto"
+        #"vga=current" # maybe should be using gfxplayload or gfxmode
         #"i915.fastboot=1"
-        "loglevel=3"
+        #"loglevel=3" # controled with consoleLogLevel above
+        "udev.log_level=3"
         "udev.log_priority=3"
         "vt.global_cursor_default=0" # disable the frame buffer console cursor
+
+        # auto only shows messages about failures if there is a significant delay in boot
+        "systemd.show_status=auto"
+        #"rd.systemd.show_status=auto"
       ];
     };
   };
