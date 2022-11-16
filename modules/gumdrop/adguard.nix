4c5129e0a06ea267edeabc421a5416ba6039dc2b
Lucas Riutzel <lriutzel@gmail.com>
2022-11-15 18:37:37 -0600

    update adguard

--- modules/gumdrop/adguard.nix
+++ modules/gumdrop/adguard.nix
@@ -10,13 +10,19 @@ in {
   config = mkIf config.gumdrop.adguard {
     services.adguardhome = {
       enable = true;
+      # opens port
       openFirewall = true;
       extraArgs = [];
       port = 80; # Web gui
       host = "10.16.1.2";
     };
 
-    networking.firewall.allowedTCPPorts = [ 53 80 ];
+    networking.firewall.allowedTCPPorts = [ 53 ];
     networking.firewall.allowedUDPPorts = [ 53 ];
+
+    # Ensures that adguardhome doesn't stop until libvirtd has
+    # This is simply to keep DNS running as long as possible if running on
+    # a machine also running libvirtd.
+    systemd.services.libvirtd.after = ["adguardhome.service"];
   };
 }
