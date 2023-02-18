f8c18afda3a047d5ea3d06b4876ffeb740088ee3
Lucas Riutzel <lriutzel@gmail.com>
2023-02-18 16:33:30 -0600

    ssh host keys are handled in secrets

--- modules/machine/impermanence.nix
+++ modules/machine/impermanence.nix
@@ -48,10 +48,6 @@ with lib;
         ];
         files = [
           "/etc/machine-id"
-          "/etc/ssh/ssh_host_ed25519_key"
-          "/etc/ssh/ssh_host_ed25519_key.pub"
-          "/etc/ssh/ssh_host_rsa_key"
-          "/etc/ssh/ssh_host_rsa_key.pub"
           { file = "/etc/nix/id_rsa"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
         ];
       };
