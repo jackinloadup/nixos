47ada3e333f6581402c388c482f05ce56b5b1f53
Lucas Riutzel <lriutzel@gmail.com>
2023-02-20 15:43:02 -0600

    format cleanup in hardware-configuration

--- machines/nat/hardware-configuration.nix
+++ machines/nat/hardware-configuration.nix
@@ -1,32 +1,34 @@
-{ self, lib, ... }:
+{ ... }:
 
 {
   imports = [
     ../../profiles/intel.nix
   ];
 
-  # Required for throttled when running on the 5.9 kernel.
-  #boot.kernelParams = [ "msr.allow_writes=on" ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ "kvm-intel" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModprobeConfig = "options snd-hda-intel enable_msi=1";
+  config = {
+    # Required for throttled when running on the 5.9 kernel.
+    #boot.kernelParams = [ "msr.allow_writes=on" ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ "kvm-intel" ];
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.extraModprobeConfig = "options snd-hda-intel enable_msi=1";
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/efi";
-    fsType = "vfat";
-    options = [ "defaults" "x-gvfs-hide" ];
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/efi";
+      fsType = "vfat";
+      options = [ "defaults" "x-gvfs-hide" ];
+    };
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      options = [ "defaults" "x-gvfs-hide" ];
+    };
+
+    swapDevices = [
+      { device = "/var/swapfile"; size = 1024; } # we could use more hard drive space
+    ];
   };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-    options = [ "defaults" "x-gvfs-hide" ];
-  };
-
-  swapDevices = [
-    { device = "/var/swapfile"; size = 1024; } # we could use more hard drive space
-  ];
 }
--- machines/riko/hardware-configuration.nix
+++ machines/riko/hardware-configuration.nix
@@ -1,40 +1,43 @@
-# to /etc/nixos/configuration.nix instead.
-{ self, pkgs, nixos-hardware, ... }: {
+{ pkgs, ... }:
+
+{
   imports = [
     ../../profiles/intel.nix
   ];
 
-  # Required for throttled when running on the 5.9 kernel.
-  #boot.kernelParams = [ "msr.allow_writes=on" ];
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ahci" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.loader.efi.canTouchEfiVariables = true;
+  config = {
+    # Required for throttled when running on the 5.9 kernel.
+    #boot.kernelParams = [ "msr.allow_writes=on" ];
+    boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ahci" "sd_mod" "sr_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-label/efi";
-    fsType = "vfat";
-    options = [
-      "defaults"
-      "x-gvfs-hide"
-      "noatime"
-      "nodiratime"
-    ];
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-label/efi";
+      fsType = "vfat";
+      options = [
+        "defaults"
+        "x-gvfs-hide"
+        "noatime"
+        "nodiratime"
+      ];
+    };
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-label/nixos";
+      fsType = "ext4";
+      options = [
+        "defaults"
+        "x-gvfs-hide"
+        "noatime"
+        "nodiratime"
+        "discard"
+      ];
+    };
+
+    boot.resumeDevice = "/dev/disk/by-label/nixos";
   };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-label/nixos";
-    fsType = "ext4";
-    options = [
-      "defaults"
-      "x-gvfs-hide"
-      "noatime"
-      "nodiratime"
-      "discard"
-    ];
-  };
-
-  boot.resumeDevice = "/dev/disk/by-label/nixos";
 }
