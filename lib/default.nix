2c0ab57ef7d2252059d3a43ea5f608d18aa542fe
Lucas Riutzel <lriutzel@gmail.com>
2023-02-25 15:55:13 -0600

    add lib functions for creating an install-iso

--- lib/default.nix
+++ lib/default.nix
@@ -36,6 +36,7 @@ rec {
   );
   nixosModules = importDirOfModules "modules";
   nixosUsers = importDirOfModules "users";
+
   mkNixosSystem = pkgs: system: hostname:
     pkgs.lib.nixosSystem {
       inherit system specialArgs;
@@ -46,6 +47,69 @@ rec {
         ++ [
           inputs.secrets.nixosModules.default
           ( rootPath + "/machines/${hostname}/configuration.nix")
+        ];
+    };
+
+  mkNixosNoModulesSystem = pkgs: system: hostname:
+    pkgs.lib.nixosSystem {
+      inherit system specialArgs;
+      modules = 
+        ( import (rootPath + "/machines/${hostname}/modules.nix") { inherit inputs; })
+        ++ [
+          inputs.secrets.nixosModules.default
+          ( rootPath + "/machines/${hostname}/configuration.nix")
+        ];
+    };
+
+  mkNixosSystemGenerator = pkgs: system: hostname:
+    nixos-generators.nixosGenerate {
+      inherit system specialArgs;
+      format = "install-iso";
+      modules = [
+          inputs.secrets.nixosModules.default
+          ( rootPath + "/modules/machine/nix.nix" )
+          ({pkgs, config, ... }: {
+            config = {
+              environment.systemPackages = [
+                pkgs.git
+                pkgs.htop
+                pkgs.tmux
+              ];
+
+              # IDK why it's needed but keep sshd running
+              services.openssh.startWhenNeeded = mkForce false;
+
+              # Make using wireless easier via nmtui-connect
+              networking.networkmanager.enable = true;
+
+              # Installer uses wpa-supplicant by default
+              networking.wireless.enable = mkForce false;
+
+              # Playing with iwd 
+              #environment.systemPackages = with pkgs; [ iwgtk ];
+              networking.networkmanager.wifi.backend = "iwd";
+              networking.wireless.iwd.enable = true;
+              networking.wireless.iwd.settings = {
+                General = {
+                  AddressRandomization = "network";
+                  AddressRandomizationRange = "full";
+                  DisableANQP = false; # Hotspot 2.0 explore turning on
+                };
+                Network = {
+                  EnableIPv6 = true;
+                  RoutePriorityOffset = 300;
+                };
+                Settings = {
+                  AutoConnect = true;
+                  AlwaysRandomizeAddress = true;
+                };
+                Scan = {
+                  InitialPeriodicScanInterval = 1;
+                  MaximumPeriodicScanInterval = 60;
+                };
+              };
+            };
+          })
         ];
     };
 }
