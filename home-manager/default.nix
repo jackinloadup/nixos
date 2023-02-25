d1b524312869c8f4f30e299bd21ffc8be1209d57
Lucas Riutzel <lriutzel@gmail.com>
2023-02-25 15:16:27 -0600

    remove unused variables

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -2,17 +2,14 @@
 
 let
   inherit (lib) mkDefault;
-  settings = import ../settings;
-  ifGraphical = nixosConfig.machine.sizeTarget > 1;
 in {
   imports = [ ];
 
   config = {
-    # You can update Home Manager without changing this value. See
-    # the Home Manager release notes for a list of state version
-    # changes in each release.
+    # Home manager will have the same stateVersion as system
     home.stateVersion = nixosConfig.system.stateVersion;
 
+    # enable management of XDG base directories
     xdg.enable = mkDefault true;
 
     programs.home-manager.enable = true;
