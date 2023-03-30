49f7a042349adbaf2d89ff10b97af13a7dcc5f5f
Lucas Riutzel <lriutzel@gmail.com>
2023-03-30 10:37:11 -0500

    zsh add nix-zsh-completions plugin

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -79,6 +79,16 @@ in {
               sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
             };
           }
+          {
+            name = "nix-zsh-completions";
+            file = "nix-zsh-completions.plugin.zsh";
+            src = pkgs.fetchFromGitHub {
+              owner = "spwhitt";
+              repo = "nix-zsh-completions";
+              rev = "0.4.4";
+              sha256 = "Djs1oOnzeVAUMrZObNLZ8/5zD7DjW3YK42SWpD2FPNk=";
+            };
+          }
         ];
       };
 
