5ce8296435310f5bb0cf792ba6bfc13690b2fd96
Lucas Riutzel <lriutzel@gmail.com>
2023-03-30 10:34:18 -0500

    zsh increase zsh-nix-shell version to 0.5.0

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -70,7 +70,7 @@ in {
             src = pkgs.fetchFromGitHub {
               owner = "chisui";
               repo = "zsh-nix-shell";
-              rev = "v0.4.0";
+              rev = "v0.5.0";
               sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
             };
           }
