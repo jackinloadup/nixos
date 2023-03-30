6a0f0fed096f086cdd5980f358c7a41e1b4087e9
Lucas Riutzel <lriutzel@gmail.com>
2023-03-30 10:44:26 -0500

    bluetooth refinement

--- modules/bluetooth/default.nix
+++ modules/bluetooth/default.nix
@@ -12,24 +12,44 @@ in {
     hardware.bluetooth = {
       enable = true;
       powerOnBoot = true;
+      disabledPlugins = [ "sap" ]; # SIM Access Profile fails and isn't needed
+      hsphfpd.enable = false; # Handled in Wireplumer
       settings = {
         General = {
           FastConnectable = "true";
           JustWorksRepairing = "always";
           #MultiProfile = "multiple";
           DiscoverableTimeout = 0;
+          # Enables D-Bus experimental interfaces
+          # Possible values: true or false
+          #Experimental = true
+
+          # Enables kernel experimental features, alternatively a list of UUIDs
+          # can be given.
+          # Possible values: true,false,<UUID List>
+          # Possible UUIDS:
+          # Defaults to false.
+          #KernelExperimental = true
         };
         Policy = {
           AutoEnable = true;
         };
       };
     };
+
+    environment.systemPackages = [ pkgs.bluetuith ];
     environment.etc."bluetooth/input.conf".text = ''
 [General]
 IdleTimeout=1
 '';
+# I can't seem to find offical documentation for what can be in these files
+# Enable field is under testing
+    environment.etc."bluetooth/audio.conf".text = ''
+[General]
+AutoConnect=true
+Enable=Source,Sink,Headset,Gateway,Control,Socket,Media
+'';
 
-    hardware.bluetooth.hsphfpd.enable = false; # Handled in Wireplumer
     # hsphfpd fails if this is enabled https://github.com/NixOS/nixpkgs/issues/114222
     systemd.user.services.telephony_client.enable = false; # work around for above
 
