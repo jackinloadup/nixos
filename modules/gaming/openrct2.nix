9c7fb354e113fb001f6dba276c15b82f72b04ced
Lucas Riutzel <lriutzel@gmail.com>
2023-04-19 12:26:37 -0500

    rebase Openrct2

--- modules/gaming/openrct2.nix
+++ modules/gaming/openrct2.nix
@@ -1,14 +1,13 @@
 { lib, pkgs, config, ... }:
 
 let
-  inherit (lib) mkIf;
+  inherit (lib) mkIf mkEnableOption genAttrs attrNames;
+  normalUsers = attrNames config.users.users;
+  addHomeFile = users: path: file: (genAttrs users (user: {home.file.${path} = file;}));
+
   cfg = config.machine;
 in {
   config = mkIf cfg.gaming {
-    environment.systemPackages = with pkgs; [
-      openrct2
-    ];
-
     # RollerCoaster Tycoon 2 openrct2 multiplayer
     networking.firewall.allowedTCPPorts = [ 11753 ];
     networking.firewall.allowedUDPPorts = [ 11753 ];
