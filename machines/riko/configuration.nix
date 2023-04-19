b78463bad4a10d44d8b9642ba4d617c2ed45a512
Lucas Riutzel <lriutzel@gmail.com>
2023-04-19 12:27:27 -0500

    move logitech mouse setting to gumdrop

--- machines/riko/configuration.nix
+++ machines/riko/configuration.nix
@@ -93,11 +93,6 @@ in {
     };
   };
 
-  # set logitec mouse to autosuspend after 60 seconds
-  services.udev.extraRules = ''
-ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTR{idProduct}=="c52b", TEST=="power/control", ATTR{power/control}:="auto", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}:="60000"
-  '';
-
   hardware.brillo.enable = true;
   services.acpid = {
     enable = true;
--- modules/gumdrop/default.nix
+++ modules/gumdrop/default.nix
@@ -16,5 +16,11 @@ in {
     # as the machine is connected to more networks
     networking.search = [ "home.lucasr.com" ];
     networking.domain = "home.lucasr.com";
+
+    # set logitec mouse to autosuspend after 60 seconds
+    services.udev.extraRules = ''
+ACTION=="add", SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTR{idProduct}=="c52b", TEST=="power/control", ATTR{power/control}:="auto", TEST=="power/autosuspend_delay_ms", ATTR{power/autosuspend_delay_ms}:="60000"
+    '';
+
   };
 }
