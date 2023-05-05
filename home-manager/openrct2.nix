37cd1fc1461e30ccb6bfd7274da269e69be7831d
Lucas Riutzel <lriutzel@gmail.com>
2023-05-05 15:21:03 -0500

    make openrct a home-manager module

--- home-manager/openrct2.nix
+++ home-manager/openrct2.nix
@@ -1,9 +1,12 @@
 { lib, pkgs, config, ... }:
 
 let
+  inherit (lib) mkIf mkEnableOption;
 
 in {
-  config = {
+  options.programs.openrct2.enable = mkEnableOption "Enable OpenRCT2";
+
+  config = mkIf config.programs.openrct2.enable {
 
     home.packages = [
       pkgs.openrct2
