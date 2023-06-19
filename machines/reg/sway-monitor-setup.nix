ea9b64e355ec4f6d350d1fd94657489b9086abc8
Lucas Riutzel <lriutzel@gmail.com>
2023-06-19 12:51:35 -0500

    adjust to new monitor setup

--- machines/reg/sway-monitor-setup.nix
+++ machines/reg/sway-monitor-setup.nix
@@ -8,11 +8,11 @@
   home-manager.users.lriutzel.wayland.windowManager.sway.config = {
     output = {
       "DP-1" = {
-        pos = "0 630";
+        pos = "3840 0";
       };
       "DP-2" = {
-        transform = "90";
-        pos = "3840 0";
+        #transform = "90";
+        pos = "0 0";
         bg = ''~/Pictures/background-virt.jpg fill'';
       };
     };
