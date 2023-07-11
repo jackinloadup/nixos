ba19dd0d2d02439633c8e67627c8cdda2fe9a9ed
Lucas Riutzel <lriutzel@gmail.com>
2023-07-11 01:47:16 -0500

    make less look better

--- modules/machine/tui.nix
+++ modules/machine/tui.nix
@@ -169,6 +169,20 @@ in {
 
     environment.sessionVariables = {
       EDITOR = "vim";
+      LESS_TERMCAP_mb="$(tput bold; tput setaf 2)";
+      LESS_TERMCAP_md="$(tput bold; tput setaf 3)";
+      LESS_TERMCAP_me="$(tput sgr0)";
+      LESS_TERMCAP_so="$(tput bold; tput setaf 3; tput setab 4)";
+      LESS_TERMCAP_se="$(tput rmso; tput sgr0)";
+      LESS_TERMCAP_us="$(tput smul; tput bold; tput setaf 2)";
+      LESS_TERMCAP_ue="$(tput rmul; tput sgr0)";
+
+      LESS_TERMCAP_mr="$(tput rev)";
+      LESS_TERMCAP_mh="$(tput dim)";
+      LESS_TERMCAP_ZN="$(tput ssubm)";
+      LESS_TERMCAP_ZV="$(tput rsubm)";
+      LESS_TERMCAP_ZO="$(tput ssupm)";
+      LESS_TERMCAP_ZW="$(tput rsupm)";
     };
 
     environment.shellAliases = {
