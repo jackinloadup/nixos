989da4ba630f6676332ce71f3d4c474b6dd178eb
Lucas Riutzel <lriutzel@gmail.com>
2023-08-22 09:09:12 -0500

    play with netdata

--- machines/reg/configuration.nix
+++ machines/reg/configuration.nix
@@ -57,6 +57,9 @@ in {
   services.kubo.enable = true;
   services.kubo.settings.Addresses.API = "/ip4/127.0.0.1/tcp/5001";
 
+  networking.firewall.allowedTCPPorts = [ 19999 ]; # netdata port;ff
+  services.netdata.enable = true; 
+
   services.pipewire.enable = true;
 
   services.rtl_433 = {
