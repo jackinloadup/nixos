7656b4588f6d2590c30c62bb83a3c99c1dfd35f3
Lucas Riutzel <lriutzel@gmail.com>
2023-09-04 12:13:15 -0500

    add description to rtlamr package

--- flake.nix
+++ flake.nix
@@ -160,6 +160,11 @@
           rev = "7926ab759fcd07021dfa37d115db4fd10f2d127c";
           hash = "sha256-d22mvVcz52yeKGebsBa+a1e5JFuybF8SOA0KGLtStw8=";
         };
+        meta = {
+          description = "SDR receiver for Itron ERT compatible smart meters";
+          homepage = "https://github.com/bemasher/rtlamr";
+          longDescription = "An rtl-sdr receiver for Itron ERT compatible smart meters operating in the 900MHz ISM band";
+        };
       };
       packages.x86_64-linux.sd-image = mkNixosSystemGenerator defaultPkgs "x86_64-linux" "lyza";
       formatter.x86_64-linux = defaultPkgs.legacyPackages.x86_64-linux.alejandra;
