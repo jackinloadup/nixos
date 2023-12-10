00b88bfebc87fbd9395d1364559bca4cae8fdd50
Lucas Riutzel <lriutzel@gmail.com>
2023-12-10 00:51:03 -0600

    refine some commandline stuff

--- modules/home-manager/tui.nix
+++ modules/home-manager/tui.nix
@@ -8,45 +8,39 @@
   inherit (lib) optionalString;
   settings = import ../../settings;
 in {
-  imports = [
-    ./base16.nix
-  ];
-
   config = {
-    home.sessionVariables = {
-      # Share environment variables between WSL and Windows
-      #TERMINFO_DIRS = "${config.home.homeDirectory}/.nix-profile/share/terminfo";
-      #WSLENV="$TERMINFO_DIRS";
-    };
+    #programs.bash.enable = true;
 
-    programs = {
-      htop.enable = true;
-
-      readline = {
-        enable = true;
-        bindings = {
-          "\\C-h" = "backward-kill-word";
-        };
-        extraConfig = ''
-          set editing-mode vi
-
-          set show-mode-in-prompt on
-          set vi-ins-mode-string "\1\e[5 q\2"
-          set vi-cmd-mode-string "\1\e[2 q\2"
-
-          set keymap vi-command
-          # j and k should search for the string of characters preceding the cursor
-          "k": history-search-backward
-          "j": history-search-forward
-
-          set keymap vi-insert
-          # inoremap jk <Esc>
-          "jk": vi-movement-mode
-        '';
-      };
-    };
-
+    programs.htop.enable = true;
     home.file."${config.xdg.configHome}/htop/htoprc".source = ./htoprc;
+    #xdg.configFile = {
+    #  source = ./htoprc;
+    #  target = "htop/hotprc";
+    #};
+
+    programs.readline = {
+      enable = true;
+      bindings = {
+        "\\C-h" = "backward-kill-word";
+      };
+      extraConfig = ''
+        set editing-mode vi
+
+        set show-mode-in-prompt on
+        set vi-ins-mode-string "\1\e[5 q\2"
+        set vi-cmd-mode-string "\1\e[2 q\2"
+
+        set keymap vi-command
+        # j and k should search for the string of characters preceding the cursor
+        "k": history-search-backward
+        "j": history-search-forward
+
+        set keymap vi-insert
+        # inoremap jk <Esc>
+        "jk": vi-movement-mode
+      '';
+    };
+
 
     home.packages = [
       # Debug / system info
