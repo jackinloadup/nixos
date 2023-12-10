a8dcc5f94df478978c2aaf5b20a2e4024b6a2181
Lucas Riutzel <lriutzel@gmail.com>
2023-12-10 00:50:20 -0600

    move many packages to lriutzel

--- modules/home-manager/tui.nix
+++ modules/home-manager/tui.nix
@@ -48,59 +48,12 @@ in {
 
     home.file."${config.xdg.configHome}/htop/htoprc".source = ./htoprc;
 
-    home.packages = with pkgs; [
+    home.packages = [
       # Debug / system info
-      iotop
-      inetutils
-      usbutils # an alternative could be busybox cope toybox
-      hwloc # can show hardware topo with lstopo
-      #unzip # duh
-      lftp # ftp client
-      terminal-colors # print all the terminal colors
-
-      # unar is HUGE at 930mb
-      #unar # An archive unpacker program GUI & TUI
-      units
-
-      sad # tool to search and replace
-      jless # json viewer
-      tealdeer # $tldr strace
-      nota # fancy cli calculator
-      #bitwarden-cli
-      python39Packages.youtube-dl # there is an alt youtube-dl-lite
-      xdg-utils # for xdg-open
-      xdg-user-dirs # command to get the path to Downloads/Pictures/ect
-      #nur.repos.ambroisie.comma # like nix-shell but more convinient
-      nixos-shell
-
-      ## spreadsheet stuffs
-      sc-im
-      visidata
-
-      # TUI to GUI helpers
-      bfs # breadth-first version of the UNIX find command. might be faster than fd?
-      broot # tree directory viewer
-      #dragon-drop # in unstable its maybe xdragon
-      ## networking
-      nethogs
-      ngrep
-      ## fast adds chromium
-      #fast-cli # bandwidth test through fast.com
-      nmap
-
-      ## Audio
-      playerctl # TUI
-
-      # Fun
-      asciiquarium # Fun aquarium animation
-      cmatrix # Fun matrix animation
-      nms # https://github.com/bartobri/no-more-secrets
-      cava # Console-based Audio Visualizer for Alsa
-      nsnake # snake game
-      terminal-parrot # parrot in your terminal
-
-      # k8s
-      k9s # Kubernetes CLI To Manage Your Clusters In Style
+      pkgs.iotop
+      pkgs.inetutils
+      pkgs.usbutils # an alternative could be busybox cope toybox
+      pkgs.hwloc # can show hardware topo with lstopo
     ];
   };
 }
