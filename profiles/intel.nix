0829dd11c69e36215e52e4b9036d4bdfa393cb74
Lucas Riutzel <lriutzel@gmail.com>
2023-12-10 01:20:22 -0600

    refine intel profile

--- profiles/intel.nix
+++ profiles/intel.nix
@@ -1,7 +1,17 @@
-{pkgs, ...}: {
+{pkgs, lib, config,  ...}: let
+  inherit (lib) optionals mkIf;
+in {
   boot = {
     initrd.kernelModules = ["kvm-intel"];
     kernelModules = ["kvm-intel"];
+    kernelParams = [
+      "nosgx" # remove "SGX disable by BIOS" message on boot
+      #"intel_iommu=on"
+    ] ++ optionals (!config.boot.initrd.verbose) [
+      # may cause loss of backlight control
+      # Should keep resolution on boot
+      "i915.fastboot=1"
+    ];
     extraModprobeConfig = "options snd-hda-intel enable_msi=1";
   };
   services.xserver.videoDrivers = ["intel"];
@@ -11,7 +21,7 @@
     vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
   };
 
-  environment.systemPackages = with pkgs; [
-    intel-gpu-tools # intel_gpu_top and others
+  environment.systemPackages = [
+    pkgs.intel-gpu-tools # intel_gpu_top and others
   ];
 }
