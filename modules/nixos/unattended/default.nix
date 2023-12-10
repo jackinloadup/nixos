9367a2ecc821147645ba85acc36fbd8b0868893f
Lucas Riutzel <lriutzel@gmail.com>
2023-12-10 01:36:41 -0600

    add more to unused unattended module

--- modules/nixos/unattended/default.nix
+++ modules/nixos/unattended/default.nix
@@ -8,7 +8,16 @@
 in {
   options.boot.unattended = mkEnableOption "Optimise for an unattended machine";
 
+  #imports = mkIf config.boot.unattended [
+  #  # unattended != headless, but it's a good start
+  #  # Define unattended as "no user interaction required"
+  #  # what I really mean is machine is unmanaged and should correct itself if
+  #  # there is a failure. 
+  #  # (inputs.nixpkgs + "nixos/modules/profiles/headless.nix")
+  #];
+
   config = mkIf config.boot.unattended {
+    # some of these values are also set in headless.
     machine.kernel = {
       rebootAfterPanic = mkForce 10;
       panicOnOOM = mkForce true;
