ee9ac0eadf338c97dee72d32602838739745b871
Lucas Riutzel <lriutzel@gmail.com>
2025-02-18 15:38:38 -0600

    disable power on wifi / bt card

--- profiles/pro-art-7800x3d.nix
+++ profiles/pro-art-7800x3d.nix
@@ -11,5 +11,12 @@ in {
     boot.kernelPackages = pkgs.linuxPackages_zen;
     #boot.kernelPackages = pkgs.zfs_unstable.latestCompatibleLinuxPackages;
     #boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+
+    # maybe not needed after 6.12.8
+    # https://www.reddit.com/r/linux/comments/1hv24zb/mt7922_no_longer_causes_kernel_panic_on_resume/
+    # https://forum.manjaro.org/t/intermittent-connection-loss-lag-spikes/174171/16
+    boot.extraModprobeConfig = ''
+      options mt7921e disable_aspm=1
+    '';
   };
 }
