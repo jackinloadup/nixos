a8014242b227eb25af324f12bd3f79c3d836614d
Lucas Riutzel <lriutzel@gmail.com>
2025-02-20 20:59:19 -0600

    add new ftp and paperless service to handle scanned documents

--- modules/nixos/gumdrop/storage-server.nix
+++ modules/nixos/gumdrop/storage-server.nix
@@ -11,6 +11,12 @@
 in {
   imports = [];
 
+  # TODO - make nfs more secure by limiting to authenticated network or changing
+  # to an authenticated transport
+
+  # once host ips are known, the nfs mount could specify clientaddr to force
+  # connection over wireguard. Currently nfs isn't choosing that interface to
+  # use. https://serverfault.com/questions/841976/how-to-specify-ipv6-address-to-use-while-mounting-nfs
   options.gumdrop.storageServer = {
     enable = mkEnableOption "Setup connection to storage server";
     idleTimeout = mkOption {
@@ -19,10 +25,12 @@ in {
       example = 300;
       description = "Disconnect after the connection is idle for x seconds";
     };
+    family = mkEnableOption "Mount family share";
     media = mkEnableOption "Mount media share";
     roms = mkEnableOption "mount roms share";
-    home = mkEnableOption "mount home share";
+    backup = mkEnableOption "mount backup";
     software = mkEnableOption "mount software share";
+    printerScanShare = mkEnableOption "mount printer scan share";
   };
 
   config = let
@@ -55,7 +63,9 @@ in {
       fileSystems = mkMerge [
         (mount {name = "media"; path = "media";})
         (mount {name = "roms"; path = "roms";})
-        (mount {name = "home"; path = "backed-up/home";})
+        (mount {name = "backup"; path = "backed-up";})
+        (mount {name = "family"; path = "backed-up/family";})
+        (mount {name = "printerScanShare"; path = "backed-up/printerScanShare";})
       ];
 
       # This service isn't needed for NFSv4. Needed pre v4
@@ -65,7 +75,8 @@ in {
         gtk.gtk3.bookmarks = [ ]
         ++ optional cfg.media "file:///mnt/gumdrop/media Media"
         ++ optional cfg.roms "file:///mnt/gumdrop/roms Roms"
-        ++ optional cfg.home "file:///mnt/gumdrop/home Backup Home";
+        ++ optional cfg.backup "file:///mnt/gumdrop/backup Backup"
+        ++ optional cfg.family "file:///mnt/gumdrop/family Family";
       }];
     };
 }
