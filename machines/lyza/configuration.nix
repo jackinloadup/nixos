bdc47e2f967ef273798a1e8dbf1b70385b77cac1
Lucas Riutzel <lriutzel@gmail.com>
2025-02-22 11:04:11 -0600

    align all lenovo m715q systems

--- machines/lyza/configuration.nix
+++ machines/lyza/configuration.nix
@@ -115,8 +115,6 @@ in {
   #  ];
   #};
 
-  # Play with TPM. Hope to have ssh host key come from tpm.
-  security.tpm2.enable = false;
   #security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
   #security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
   #users.users.lriutzel.extraGroups = [ "tss" ];  # tss group has access to TPM devices
--- machines/timberlake/hardware-configuration.nix
+++ machines/timberlake/hardware-configuration.nix
@@ -1,6 +1,6 @@
 {flake, pkgs, config, lib, ...}: {
   imports = [
-    ../../profiles/amd.nix
+    ../../profiles/lenovo-m715q.nix
     (import ../../profiles/disk-workstation-3.nix {
       inherit flake pkgs config lib;
       device = "/dev/nvme0n1";
@@ -9,11 +9,5 @@
   ];
 
   config = {
-    boot.initrd.availableKernelModules = [ "usbhid" "ehci_pci" "nvme" "xhci_pci" "ahci" ];
-    boot.loader.efi.canTouchEfiVariables = true;
-    #boot.extraModprobeConfig = "options snd-hda-intel enable_msi=1";
-    boot.kernelModules = [ "kvm-amd" "amd_gpio" ];
-
-    nixpkgs.hostPlatform = "x86_64-linux";
   };
 }
