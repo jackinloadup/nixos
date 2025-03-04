848072a17665a8a03aa697f096c31354b30486c8
Lucas Riutzel <lriutzel@gmail.com>
2025-03-04 15:23:32 -0600

    impliment ai chat on reg and expose it via marulk

--- machines/reg/configuration.nix
+++ machines/reg/configuration.nix
@@ -143,6 +143,81 @@ in {
     services.displayManager.autoLogin.enable = true;
     services.displayManager.autoLogin.user = "lriutzel";
     services.displayManager.defaultSession = "hyprland";
+
+
+    # ollama.home.lucasr.com in mirotik static dns
+    services.ollama = {
+      enable = true;
+      rocmOverrideGfx = "11.0.0"; ## rdna 3 11.0.0
+      acceleration = "rocm";
+      openFirewall = true;
+      host = "ollama.home.lucasr.com";
+      home = "/var/lib/private/ollama";
+      user = "ollama";
+      loadModels = [
+        "deepseek-r1"
+        "llama3.2:1b"
+        "codellama"
+        "gemma2:9b"
+        #"gemma2:27b" # too big
+        "mistral"
+        "moondream"
+        "starling-lm"
+        "solar"
+        "llava"
+        "llama2-uncensored"
+        "phi4"
+        "phi3"
+      ];
+
+      package = pkgs.unstable.ollama;
+
+      environmentVariables = {
+        OLLAMA_ORIGINS = "*";
+      };
+    };
+
+    # ollama.home.lucasr.com in mirotik static dns
+    services.open-webui = {
+      enable = true;
+      openFirewall = true;
+      port = 11112;
+      stateDir = "/var/lib/private/open-webui";
+      host = "chat.lucasr.com";
+      environment = {
+        # PYDANTIC_SKIP_VALIDATING_CORE_SCHEMAS = "True";
+        OLLAMA_BASE_URL = "http://ollama.home.lucasr.com:${toString config.services.ollama.port}";
+        ENABLE_OLLAMA_API = "true";
+        DEFAULT_USER_ROLE = "user";
+        WEBUI_AUTH = "False";
+        #WEBUI_AUTH_TRUSTED_EMAIL_HEADER = "X-Webauth-Email";
+        #WEBUI_AUTH_TRUSTED_NAME_HEADER = "X-Webauth-Name";
+        ENABLE_OAUTH_SIGNUP = "false";
+        ENABLE_SIGNUP = "false";
+        WEBUI_URL = "https://chat.lucasr.com";
+        OAUTH_MERGE_ACCOUNTS_BY_EMAIL = "false";
+
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+      };
+    };
+
+    #services.vaultwarden = {
+    #  enable = false;
+    #  environmentFile = config.age.secrets.vaultwarden-env.path;
+    #  #backupDir = "/var/lib/vw-backup";
+
+    #  config = {
+    #    DOMAIN = "https://bw.9000.dev";
+    #    SIGNUPS_ALLOWED = "false";
+    #    PASSWORD_HINTS_ALLOWED = "false";
+    #    ROCKET_ADDRESS = "127.0.0.1";
+    #    ROCKET_PORT = 8222;
+    #    PASSWORD_ITERATIONS = 600000;
+    #  };
+    #};
+
     # Login Manager
     services.xserver.displayManager.gdm.enable = true;
     #services.xserver.enable = true;
