e8e894ef198cfd34fa52f746ca63d3dfa5e9e39e
Lucas Riutzel <lriutzel@gmail.com>
2025-03-23 16:42:33 -0500

    add ucodenix to amd profile

--- profiles/amd.nix
+++ profiles/amd.nix
@@ -1,64 +1,75 @@
-{pkgs, lib, config,  ...}: let
+{pkgs, lib, config, flake,  ...}: let
   inherit (lib) optionals mkIf;
 in {
-  boot = {
-    initrd.kernelModules = ["kvm-amd" "amdgpu"];
-    kernelModules = ["kvm-amd" "amdgpu" "amd_gpio" ];
-    #kernelParams = [
-    #  #"nosgx" # remove "SGX disable by BIOS" message on boot
-    #  #"intel_iommu=on"
-    #] ++ optionals (!config.boot.initrd.verbose) [
-    #  # may cause loss of backlight control
-    #  # Should keep resolution on boot
-    #  #"i915.fastboot=1"
-    #];
-    #extraModprobeConfig = "options snd-hda-intel enable_msi=1";
-  };
-  services.xserver.videoDrivers = ["amdgpu"];
+  imports = [ flake.inputs.ucodenix.nixosModules.default ];
 
-  # forgot what this was for
-  #nixpkgs.config.packageOverrides = pkgs: {
-  #  vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
-  #};
+  config = {
+    boot = {
+      initrd.kernelModules = ["kvm-amd" "amdgpu"];
+      kernelModules = ["kvm-amd" "amdgpu" "amd_gpio" ];
+      #kernelParams = [
+      #  #"nosgx" # remove "SGX disable by BIOS" message on boot
+      #  #"intel_iommu=on"
+      #] ++ optionals (!config.boot.initrd.verbose) [
+      #  # may cause loss of backlight control
+      #  # Should keep resolution on boot
+      #  #"i915.fastboot=1"
+      #];
+      #extraModprobeConfig = "options snd-hda-intel enable_msi=1";
+    };
+    services.xserver.videoDrivers = ["amdgpu"];
+
+    #nixpkgs.config.rocmSupport = true;
+
+    # maybe useful? opencl support using rocm
+    #hardware.amdgpu.opencl.enable = true;
+
+    # forgot what this was for
+    #nixpkgs.config.packageOverrides = pkgs: {
+    #  vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+    #};
 
 
-  # use amdvlk drivers instead mesa radv drivers
-  # maybe mesa dribers are better??
-  ##hardware.amdgpu.amdvlk.enable = true;
+    # use amdvlk drivers instead mesa radv drivers
+    # maybe mesa dribers are better??
+    ##hardware.amdgpu.amdvlk.enable = true;
 
-  #environment.variables = {
-  #  AMD_DEBUG = "nodcc";
-  #};
+    #environment.variables = {
+    #  AMD_DEBUG = "nodcc";
+    #};
 
-  #hardware.opengl.driSupport32Bit = true;
-  hardware.graphics = {
-    # antiquated in 24.11
-    #driSupport = true;
-    #driSupport32Bit = true;
+    #hardware.opengl.driSupport32Bit = true;
+    hardware.graphics = {
+      # antiquated in 24.11
+      #driSupport = true;
+      #driSupport32Bit = true;
 
-    extraPackages = [
-      #pkgs.amdvlk # causing large black border around gnome apps
-      #pkgs.rocm-opencl-icd
-      #pkgs.rocm-opencl-runtime
-      # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/amd/default.nix
-      #pkgs.rocmPackages.clr
+      extraPackages = [
+        #pkgs.amdvlk # causing large black border around gnome apps
+        #pkgs.rocm-opencl-icd
+        #pkgs.rocm-opencl-runtime
+        # https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/amd/default.nix
+        #pkgs.rocmPackages.clr
+      ];
+
+      # For 32 bit applications
+      extraPackages32 = [
+        pkgs.driversi686Linux.amdvlk
+      ];
+    };
+
+
+    # some of these could be placed into home-manager?
+    # disabled because this is added onto servers which don't' need them
+    environment.systemPackages = [
+      #pkgs.ryzenadj # not in use
+      #pkgs.zenstates # not in use
+      pkgs.radeontop #  Top for amd cards. Could maybe be placed somewhere else? debug only if possible?
+      #pkgs.radeon-profile # gui - need to figure out how to grant correct permissions
+      pkgs.nvtopPackages.amd
     ];
 
-    # For 32 bit applications
-    extraPackages32 = [
-      pkgs.driversi686Linux.amdvlk
-    ];
+    services.ucodenix.enable = true;
   };
-
-
-  # some of these could be placed into home-manager?
-  # disabled because this is added onto servers which don't' need them
-  environment.systemPackages = [
-    #pkgs.ryzenadj # not in use
-    #pkgs.zenstates # not in use
-    pkgs.radeontop #  Top for amd cards. Could maybe be placed somewhere else? debug only if possible?
-    #pkgs.radeon-profile # gui - need to figure out how to grant correct permissions
-    pkgs.nvtopPackages.amd
-  ];
 }
 
