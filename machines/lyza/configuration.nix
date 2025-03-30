0450160a30e6993a4c090d9ed5b1bcb3903c592e
Lucas Riutzel <lriutzel@gmail.com>
2025-03-30 15:02:23 -0500

    remove unused lyza conifg

--- machines/lyza/configuration.nix
+++ machines/lyza/configuration.nix
@@ -82,14 +82,6 @@ in {
 
   services.fwupd.enable = mkForce true;
 
-  # doesn't seem to work
-  #services.create_ap.enable = true;
-  #services.create_ap.settings = {
-  #  INTERNET_IFACE = "enp1s0";
-  #  SSID = "ArtSpot";
-  #  WIFI_IFACE = "wlan0";
-  #};
-
   services.k3s.enable = false;
   services.k3s.role = "server";
   services.k3s.clusterInit = true;
@@ -99,22 +91,8 @@ in {
   #services.tor.enable = mkForce true;
   #services.tor.client.enable = mkForce true;
 
-  #services.xserver.displayManager.autoLogin.user = "lriutzel";
-  #services.xserver.displayManager.defaultSession = "sway";
-
   services.xserver.desktopManager.gnome.enable = isUserFacing;
   services.xserver.displayManager.gdm.enable = isUserFacing;
-
-  #services.getty = {
-  #  autologinUser = "kodi";
-  #  extraArgs = [
-  #    "--noissue"
-  #    "--noclear"
-  #    "--nohints"
-  #    "--nohostname"
-  #    "--skip-login"
-  #  ];
-  #};
 
   #security.tpm2.pkcs11.enable = true;  # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
   #security.tpm2.tctiEnvironment.enable = true;  # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
@@ -151,47 +129,10 @@ in {
 
   #nix.settings.max-jobs = mkDefault 8;
 
-  nixpkgs = {
-    overlays = [
-      flake.inputs.nur.overlays.default
-      flake.inputs.self.overlays.default
-      #    inputs.self.overlays.plymouth-no-gtk
-      #    inputs.self.overlays.pipewire-minimal
-    ];
-  };
-
-  virtualisation = rec {
-    vmVariant = {
-      networking.hostName = mkForce "lyzavm";
-      #services.xserver.displayManager.defaultSession = mkForce "none+i3";
-      boot.initrd.kernelModules = [];
-
-      virtualisation = {
-        #useEFIBoot = true;
-        #useBootLoader = true;
-
-        # https://github.com/NixOS/nixpkgs/issues/59219
-        cores = 4;
-        graphics = true;
-        memorySize = 2048;
-        qemu.networkingOptions = ["-nic bridge,br=br0,model=virtio-net-pci,mac=30:9c:23:01:2f:82,helper=/run/wrappers/bin/qemu-bridge-helper"];
-        qemu.options = [
-          #"-device virtio-gpu-pci"
-          #"-device virtio-gpu-gl-pci"
-          "-device virtio-vga-gl"
-          "-display gtk,gl=on"
-          "-vga none"
-        ];
-      };
-
-      networking.interfaces = mkForce {
-        eth0.useDHCP = true;
-        enp1s0f0.useDHCP = true;
-      };
-    };
-
-    vmVariantWithBootLoader = vmVariant;
-  };
+  nixpkgs.overlays = [
+    flake.inputs.nur.overlays.default
+    flake.inputs.self.overlays.default
+  ];
 
   networking = {
     hostName = "lyza";
@@ -205,30 +146,6 @@ in {
     #interfaces.enp1s0.useDHCP = true;
   };
   networking.networkmanager.enable = true;
-  networking.wireless.enable = mkForce false;
-  # Playing with iwd
-  #environment.systemPackages = with pkgs; [ iwgtk ];
-  networking.networkmanager.wifi.backend = "iwd";
-  networking.wireless.iwd.enable = true;
-  networking.wireless.iwd.settings = {
-    General = {
-      AddressRandomization = "network";
-      AddressRandomizationRange = "full";
-      DisableANQP = false; # Hotspot 2.0 explore turning on
-    };
-    Network = {
-      EnableIPv6 = true;
-      RoutePriorityOffset = 300;
-    };
-    Settings = {
-      AutoConnect = true;
-      AlwaysRandomizeAddress = true;
-    };
-    Scan = {
-      InitialPeriodicScanInterval = 1;
-      MaximumPeriodicScanInterval = 60;
-    };
-  };
 
   # clean logs older than 2d
   services.cron.systemCronJobs = [
