fec448a5e6445e9162c9080e41d0f265a57f5d6e
Lucas Riutzel <lriutzel@gmail.com>
2025-03-30 15:13:50 -0500

    inital frigate support

--- machines/lyza/frigate.nix
+++ machines/lyza/frigate.nix
@@ -1,18 +1,59 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   inherit (lib) mkIf;
 in {
   config = {
     services.frigate = {
       enable = true;
+# 0.15 doesn't appear in nixpkgs-unstable
+#package = pkgs.unstable.frigate;
+      # 0.0.0.0 didn't seem to work
       hostname = "127.0.0.1";
       #vaapiDriver = "radeonsi";
       settings = {
+#        auth = {
+# doesn't appear in 0.14.1
+#enable = false; # make things easy for now. Don't really need it.
+#reset_admin_password = true;
+#        };
+
+        ffmpeg.hwaccel_args = "preset-vaapi";
+
         mqtt = {
           enabled = true;
-          host = "127.0.0.1:1883";
+          host = "127.0.0.1";
+          port = 1883;
+          user = "mosquitto";
+          password = "mosquitto";
         };
-        cameras = { };
+
+        cameras = {
+          front = {
+            webui_url = "http://camera1";
+            detect = {
+              width = 2560;
+              height = 1920;
+              fps = 5;
+            };
+            ffmpeg = {
+              inputs = [
+                {
+                  path = "rtsp://admin:469521@camera1:554/h264Preview_01_main";
+                  roles = [ "record" ];
+                }
+                {
+                  path = "rtsp://admin:469521@camera1:554/h264Preview_01_sub";
+                  roles = [ "detect" ];
+                }
+              ];
+            };
+            motion = {
+              mask = [
+                "0.000,0.427,0.002,0.000,0.999,0.000,0.999,0.781,0.885,0.456,0.700,0.424,0.701,0.311,0.507,0.294,0.453,0.347,0.451,0.400"
+              ];
+            };
+          };
+        };
 
         detectors = {
           coral = {
@@ -20,6 +61,7 @@ in {
             device = "usb";
           };
         };
+
         record = {
           enabled = true;
           retain = {
@@ -32,27 +74,8 @@ in {
 
         snapshots = {
           enabled = true;
-          retain.default = 30;
+          retain.default = 30; # days
         };
-
-#ffmpeg:
-#  hwaccel_args: preset-rpi-64-h264
-
-#
-#cameras:
-#  name_of_your_camera:
-#    detect:
-#      width: 1280
-#      height: 720
-#      fps: 5
-#    ffmpeg:
-#      inputs:
-#        - path: rtsp://10.0.10.10:554/rtsp
-#          roles:
-#            - detect
-#    motion:
-#      mask:
-#        - 0.000,0.427,0.002,0.000,0.999,0.000,0.999,0.781,0.885,0.456,0.700,0.424,0.701,0.311,0.507,0.294,0.453,0.347,0.451,0.400
       };
     };
   };
