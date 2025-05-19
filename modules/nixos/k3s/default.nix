31e443f24affacecb437e5935e731107f1c5bb58
Lucas Riutzel <lriutzel@gmail.com>
2025-05-19 08:35:53 -0500

    add some k8s tools for future reference

--- modules/nixos/k3s/default.nix
+++ modules/nixos/k3s/default.nix
@@ -9,6 +9,11 @@ in {
   config = mkIf config.services.k3s.enable {
     environment.systemPackages = [
       pkgs.kubectl
+      # pkgs.kty # not used yet
+      # pkgs.lens # not used and closed source
+      # pkgs.seabird # native desktop app that simplifies working with Kubernetes
+      # pkgs.k9s
+      # pkgs.kdash
     ];
 
     # This is required so that pod can reach the API server (running on port 6443 by default)
