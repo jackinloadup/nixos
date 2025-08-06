a6accc49ade233b23f4c707ac0750ce3a1e626a3
Lucas Riutzel <lriutzel@gmail.com>
2025-08-06 18:10:48 -0500

    setup media services

--- modules/nixos/media/default.nix
+++ modules/nixos/media/default.nix
@@ -6,7 +6,11 @@
 }: let
 inherit (lib) mkIf;
   cfg = config.services.media-services;
-  subnet = "10.100.0.0/24";
+  allowSubnets = ''
+    allow 127.0.0.1;
+    allow 10.100.0.0/24;
+    allow 10.16.0.0/16;
+  '';
 in {
   imports = [
   ];
@@ -24,17 +28,83 @@ in {
 
   config = mkIf cfg.enable {
 
-    # places all services into a container to add another layer of security
-    containers.media-services = {
-      autoStart = true;
-      privateNetwork = true;
-      hostBridge = "br0";
-#hostAddress = "10.16.100.10";
-      localAddress = "10.16.1.10";
-      #hostAddress6 = "fc00::1";
-      #localAddress6 = "fc00::2";
+    hardware.amdgpu.opencl.enable = true;
+    hardware.amdgpu.amdvlk.enable = true;
+    hardware.graphics.extraPackages = [ pkgs.amf ];
+    hardware.graphics.enable = true;
 
-      config = { config, pkgs, lib, ... }: {
+    #readarr
+    #whisparr
+
+    # force dir to exist
+    systemd.tmpfiles.rules = [
+      "d /var/lib/sabnzbd 0775 ${config.services.sabnzbd.user} ${config.services.sabnzbd.group} -"
+      "d /var/lib/radarr 0775 ${config.services.radarr.user} ${config.services.radarr.group} -"
+      "d /var/lib/sonarr 0775 ${config.services.sonarr.user} ${config.services.sonarr.group} -"
+      "d /var/lib/lidarr 0775 ${config.services.lidarr.user} ${config.services.lidarr.group} -"
+      "d /var/lib/bazarr 0775 ${config.services.bazarr.user} ${config.services.bazarr.group} -"
+      "d /var/lib/prowlarr 0775 root root -"
+      "d /var/lib/jellyseer 0775 ${config.services.jellyfin.user} ${config.services.jellyfin.group} -"
+    ];
+
+    # places all services into a container to add another layer of security
+    #    containers.media-services = {
+    #      autoStart = true;
+    #      privateNetwork = false;
+    #      #hostBridge = "br0";
+    ##hostAddress = "10.16.100.10";
+    #      #localAddress = "10.16.1.10";
+    #      #hostAddress6 = "fc00::1";
+    #      #localAddress6 = "fc00::2";
+    #      bindMounts = {
+    #        #"/run/autobrr.secret".hostPath = "/run/autobrr.secret";
+    #        #"/var/lib/private/autobrr" = {
+    #        #  hostPath = "/var/lib/autobrr";
+    #        #  isReadOnly = false;
+    #        #};
+    #        "/var/lib/sabnzbd" = {
+    #          hostPath = "/var/lib/sabnzbd";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/radarr" = {
+    #          hostPath = "/var/lib/radarr";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/sonarr" = {
+    #          hostPath = "/var/lib/sonarr";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/lidarr" = {
+    #          hostPath = "/var/lib/lidarr";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/bazarr" = {
+    #          hostPath = "/var/lib/bazarr";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/prowlarr" = {
+    #          hostPath = "/var/lib/prowlarr";
+    #          isReadOnly = false;
+    #        };
+    #        "/var/lib/jellyseerr" = {
+    #          hostPath = "/var/lib/jellyseerr";
+    #          isReadOnly = false;
+    #        };
+    #        "/mnt/gumdrop/media" = {
+    #          hostPath = "/mnt/gumdrop/media";
+    #          isReadOnly = false;
+    #        };
+    #        #"/var/lib/qbittorrent" = {
+    #        #  hostPath = "/var/lib/qbittorrent";
+    #        #  isReadOnly = false;
+    #        #};
+    #        #"/mnt/Media" = {
+    #        #  hostPath = "/mnt/Media";
+    #        #  isReadOnly = false;
+    #        #};
+    #      };
+    #
+    #      config = { config, pkgs, lib, ... }: {
         # Set-up media group
         users.users.media = {
           isNormalUser = false;
@@ -49,45 +119,49 @@ in {
           8989
         ];
 
-        # download series
-#        services.sonarr = {
-#          enable = true;
-#          group = "media";
-#          user = "media";
-#          dataDir = "/var/lib/sonarr";
-#        };
-
-          # download movies
-        services.radarr = {
+        services.sabnzbd = { # nzb downloader
           enable = true;
           group = "media";
           user = "media";
-          dataDir = "/var/lib/radarr";
+        };
+
+        services.sonarr = { # Series
+          enable = true;
+          group = "media";
+          user = "media";
+          #  dataDir = "/var/lib/sonarr";
+        };
+
+        services.radarr = { # Movies
+          enable = true;
+          group = "media";
+          user = "media";
+          #dataDir = "/var/lib/radarr";
         };
 
           # better indexer apis
-        services.prowlarr = {
+        services.prowlarr = { # Index proxy
           enable = true;
 #group = "media";
 #          user = "media";
         };
 
-#        services.jellyseerr = {
-#          enable = true;
-#          port = 5055;
-#        };
-        services.bazarr = {
+        services.jellyseerr = { # User Friendly media request ui for external users
+          enable = true;
+          port = 5055;
+        };
+        services.bazarr = { # Subtitles
           enable = true;
           user = "media";
           group = "media";
           listenPort = 6767;
         };
-        services.lidarr = {
-          enable = true;
-          user = "media";
-          group = "media";
-          dataDir = "/var/lib/lidarr";
-        };
+        #        services.lidarr = { # Music
+        #          enable = true;
+        #          user = "media";
+        #          group = "media";
+        #          #dataDir = "/var/lib/lidarr";
+        #        };
 
         #permown."/media/arr" = {
         #  owner = "media";
@@ -110,53 +184,118 @@ in {
 #          };
 
         #});
-
-    };
-  };
+#      };
+#    };
 
     # proxy interfaces
     services.nginx.enable = true;
     services.nginx.virtualHosts = {
-      "radarr.lucasr.com" = {
+      "sabnzb.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
         extraConfig = ''
-          allow ${subnet};
+          ${allowSubnets}
           deny all;
         '';
         locations."/" = {
-          proxyPass = "http://localhost:7878";
+          proxyPass = "http://127.0.0.1:8080/";
+          proxyWebsockets = true;
+        };
+      };
+      "radarr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
+        extraConfig = ''
+          ${allowSubnets}
+          deny all;
+        '';
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.radarr.settings.server.port }/";
           proxyWebsockets = true;
         };
       };
       "sonarr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
         extraConfig = ''
-          allow ${subnet};
+          ${allowSubnets}
           deny all;
         '';
         locations."/" = {
-          proxyPass = "http://localhost:8989";
+          proxyPass = "http://127.0.0.1:${toString config.services.sonarr.settings.server.port }/";
           proxyWebsockets = true;
         };
       };
       "prowlarr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
         extraConfig = ''
-          allow ${subnet};
+          ${allowSubnets}
           deny all;
         '';
         locations."/" = {
-          proxyPass = "http://localhost:9696";
+          proxyPass = "http://127.0.0.1:${toString config.services.prowlarr.settings.server.port }/";
           proxyWebsockets = true;
         };
       };
       "jellyseerr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
         extraConfig = ''
-          allow ${subnet};
+          ${allowSubnets}
           deny all;
         '';
         locations."/" = {
-          proxyPass = "http://localhost:${toString config.services.jellyseerr.port}";
+          proxyPass = "http://127.0.0.1:${toString config.services.jellyseerr.port}";
+          proxyWebsockets = true;
+        };
+      };
+      "lidarr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
+        extraConfig = ''
+          ${allowSubnets}
+          deny all;
+        '';
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.lidarr.settings.server.port }";
+          proxyWebsockets = true;
+        };
+      };
+      "bazarr.lucasr.com" = {
+        forceSSL = true;
+        enableACME = true;
+        acmeRoot = null; # Use DNS Challenege
+
+        extraConfig = ''
+          ${allowSubnets}
+          deny all;
+        '';
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.bazarr.listenPort }";
           proxyWebsockets = true;
         };
       };
     };
+
+    #systemd.services."container@arr-servers" = {
+    #  bindsTo = ["mnt-Home.mount" "mnt-Media.mount"];
+    #  after = ["mnt-Home.mount" "mnt-Media.mount"];
+    #  unitConfig = {
+    #    ConditionPathExists = "/run/autobrr.secret";
+    #  };
+    #};
   };
 }
