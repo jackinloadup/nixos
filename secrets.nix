b7b1dc5333e46029e899df5b8d89ad648f052bc0
Lucas Riutzel <lriutzel@gmail.com>
2025-10-01 03:54:38 -0500

    add secrets for lyza frigate

--- secrets.nix
+++ secrets.nix
@@ -69,6 +69,9 @@ in
   "secrets/services/immich/secretsFile".publicKeys = servers;
 
   "secrets/commonPass.age".publicKeys = all;
+
+  "secrets/machines/lyza/frigate/environment.age".publicKeys = servers;
+
   "secrets/system/wireless-networking.age".publicKeys = all;
 
   # service users
