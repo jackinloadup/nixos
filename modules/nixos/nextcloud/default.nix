b2e20277c2aecfb14ffc7d6306d2d9936e0ec6a2
Lucas Riutzel <lriutzel@gmail.com>
2025-12-03 23:57:22 -0600

    move collabora into nextcloud module

--- modules/nixos/nextcloud/default.nix
+++ modules/nixos/nextcloud/default.nix
@@ -252,5 +252,58 @@ in {
         ${config.services.nextcloud.hostName}.email = "lriutzel@gmail.com";
       };
     };
+
+    services.collabora-online = {
+      enable = true;
+      port = 9980; # default
+      settings = {
+        # Rely on reverse proxy for SSL
+        ssl = {
+          enable = false;
+          termination = true;
+        };
+
+        # Listen on loopback interface only, and accept requests from ::1
+        net = {
+          listen = "loopback";
+          post_allow.host = ["::1"];
+        };
+
+        # Restrict loading documents from WOPI Host nextcloud.example.com
+        storage.wopi = {
+          "@allow" = true;
+          host = ["nextcloud.lucasr.com"];
+        };
+
+        # Set FQDN of server
+        server_name = "collabora.lucasr.com";
+      };
+    };
+
+    systemd.services.nextcloud-config-collabora = let
+      inherit (config.services.nextcloud) occ;
+
+      wopi_url = "http://[::1]:${toString config.services.collabora-online.port}";
+      public_wopi_url = "https://collabora.lucasr.com";
+      wopi_allowlist = lib.concatStringsSep "," [
+        "127.0.0.1"
+        "::1"
+        "10.16.1.0/24" # pretty sure these two are needed
+        "10.100.0.0/24"
+      ];
+    in {
+      wantedBy = ["multi-user.target"];
+      after = ["nextcloud-setup.service" "coolwsd.service"];
+      requires = ["coolwsd.service"];
+      script = ''
+        ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_url --value ${lib.escapeShellArg wopi_url}
+        ${occ}/bin/nextcloud-occ config:app:set richdocuments public_wopi_url --value ${lib.escapeShellArg public_wopi_url}
+        ${occ}/bin/nextcloud-occ config:app:set richdocuments wopi_allowlist --value ${lib.escapeShellArg wopi_allowlist}
+        ${occ}/bin/nextcloud-occ richdocuments:setup
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+      };
+    };
   };
 }
