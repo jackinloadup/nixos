9cf2842cb9fa981023c3b20099627ca7ebbac7e5
Lucas Riutzel <lriutzel@gmail.com>
2025-12-07 09:04:26 -0600

    nvim avante perform version migration

--- modules/home-manager/nixvim/default.nix
+++ modules/home-manager/nixvim/default.nix
@@ -242,56 +242,96 @@
             system_prompt = ''
               You are an excellent programming expert.
             '';
-            ollama = {
-              endpoint = "http://ollama.home.lucasr.com:${toString config.services.ollama.port}";
-              model = "codellama:latest";
-              #model = "qwq:32b";
+            providers = {
+              ollama = {
+                endpoint = "http://ollama.home.lucasr.com:${toString config.services.ollama.port}";
+                model = "codellama:latest";
+                #model = "qwq:32b";
+              };
+              #openai = {
+              #  endpoint = "https://api.openai.com/v1";
+              #  model = "gpt-4o";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #copilot = {
+              #  endpoint = "https://api.githubcopilot.com";
+              #  model = "gpt-4o-2024-05-13";
+              #  proxy = null;
+              #  allow_insecure = false;
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #azure = {
+              #  endpoint = "";
+              #  deployment = "";
+              #  api_version = "2024-06-01";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #claude = {
+              #  endpoint = "https://api.anthropic.com";
+              #  model = "claude-3-5-sonnet-20240620";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 8000;
+              #};
+              #gemini = {
+              #  endpoint = "https://generativelanguage.googleapis.com/v1beta/models";
+              #  model = "gemini-1.5-flash-latest";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #cohere = {
+              #  endpoint = "https://api.cohere.com/v1";
+              #  model = "command-r-plus-08-2024";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #copilot = {
+              #  endpoint = "https://api.githubcopilot.com";
+              #  model = "gpt-4o-2024-05-13";
+              #  proxy = null;
+              #  allow_insecure = false;
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #azure = {
+              #  endpoint = "";
+              #  deployment = "";
+              #  api_version = "2024-06-01";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #claude = {
+              #  endpoint = "https://api.anthropic.com";
+              #  model = "claude-3-5-sonnet-20240620";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 8000;
+              #};
+              #gemini = {
+              #  endpoint = "https://generativelanguage.googleapis.com/v1beta/models";
+              #  model = "gemini-1.5-flash-latest";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
+              #cohere = {
+              #  endpoint = "https://api.cohere.com/v1";
+              #  model = "command-r-plus-08-2024";
+              #  timeout = 30000;
+              #  temperature = 0;
+              #  max_tokens = 4096;
+              #};
             };
-            #openai = {
-            #  endpoint = "https://api.openai.com/v1";
-            #  model = "gpt-4o";
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 4096;
-            #};
-            #copilot = {
-            #  endpoint = "https://api.githubcopilot.com";
-            #  model = "gpt-4o-2024-05-13";
-            #  proxy = null;
-            #  allow_insecure = false;
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 4096;
-            #};
-            #azure = {
-            #  endpoint = "";
-            #  deployment = "";
-            #  api_version = "2024-06-01";
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 4096;
-            #};
-            #claude = {
-            #  endpoint = "https://api.anthropic.com";
-            #  model = "claude-3-5-sonnet-20240620";
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 8000;
-            #};
-            #gemini = {
-            #  endpoint = "https://generativelanguage.googleapis.com/v1beta/models";
-            #  model = "gemini-1.5-flash-latest";
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 4096;
-            #};
-            #cohere = {
-            #  endpoint = "https://api.cohere.com/v1";
-            #  model = "command-r-plus-08-2024";
-            #  timeout = 30000;
-            #  temperature = 0;
-            #  max_tokens = 4096;
-            #};
             vendors = { };
             behaviour = {
               auto_suggestions = false;
